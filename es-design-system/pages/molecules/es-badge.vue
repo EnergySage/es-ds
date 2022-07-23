a7273c6d1becd2cb826f7a9a11d895856756fc81
Ryan Mulready <ryan.mulready@gmail.com>
2022-07-22 20:01:12 -0400

    Update badge docs

--- es-design-system/pages/molecules/es-badge.vue
+++ es-design-system/pages/molecules/es-badge.vue
@@ -1,7 +1,24 @@
 <template>
-    <es-badge>
-        Test
-    </es-badge>
+    <div>
+        <es-badge>
+            Primary
+        </es-badge>
+        <es-badge variant="success">
+            Success
+        </es-badge>
+        <es-badge variant="danger">
+            Danger
+        </es-badge>
+        <es-badge variant="warning">
+            Warning
+        </es-badge>
+        <es-badge variant="info">
+            Info
+        </es-badge>
+        <es-badge variant="dark">
+            Dark
+        </es-badge>
+    </div>
 </template>
 <script>
 import { EsBadge } from '@energysage/es-vue-base';
