bdf515a84443e5b3d78e134351eade04f13b21b4
Ryan Mulready <ryan.mulready@gmail.com>
2022-07-29 09:44:32 -0400

    Move breadcrumbs defs to es-bs-base

--- es-vue-base/src/lib-components/EsBreadcrumbs.vue
+++ es-vue-base/src/lib-components/EsBreadcrumbs.vue
@@ -18,21 +18,8 @@ export default {
 @import '~@energysage/es-bs-extends/scss/includes';
 
 .breadcrumb {
-    padding: 0;
-    margin: 0;
-
-    .active {
+    .active span {
         font-weight: $font-weight-bold;
-        margin-right: 0;
-    }
-
-    .breadcrumb-item {
-        margin-right: 0.5rem;
-        padding-left: 0;
-
-       &::before {
-            padding-right: 0.5rem;
-        }
     }
 }
 </style>
