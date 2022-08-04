963fd88788a25aecf78b33f6b9dcd9a81cf19467
Ryan Mulready <ryan.mulready@gmail.com>
2022-08-04 15:01:06 -0400

    Update espopover docs

--- es-design-system/pages/molecules/es-popover.vue
+++ es-design-system/pages/molecules/es-popover.vue
@@ -1,31 +1,55 @@
 <template>
     <div>
-        <!-- eslint-disable-next-line vuejs-accessibility/label-has-for -->
-        <label>
-            Click Me
-            <es-button
-                id="myTarget"
-                class="p-0 text-gray-700 align-text-bottom"
-                tabindex="0"
-                variant="link">
-                <IconInfo
-                    id="myTarget"
-                    width="16px"
-                    height="16px" />
-            </es-button>
-        </label>
-        <EsPopover target="myTarget">
-            <template #title>
-                My Title
-            </template>
-            <p>
-                Install solar panels through this program and get $250 cash back.
-                <a
-                    class="mt-3 d-block"
-                    href="https://communitysolar.energysage.com/"
-                    target="_blank">Learn more.</a>
-            </p>
-        </EsPopover>
+        <div>
+            <!-- eslint-disable vuejs-accessibility/label-has-for -->
+            <label>
+                With Title
+                <es-button
+                    id="titleTarget"
+                    class="p-0 text-gray-700 align-text-bottom"
+                    tabindex="0"
+                    variant="link">
+                    <IconInfo
+                        width="16px"
+                        height="16px" />
+                </es-button>
+            </label>
+            <EsPopover target="titleTarget">
+                <template #title>
+                    My Title
+                </template>
+                <p>
+                    Install solar panels through this program and get $250 cash back.
+                    <a
+                        class="mt-3 d-block"
+                        href="https://communitysolar.energysage.com/"
+                        target="_blank">Learn more.</a>
+                </p>
+            </EsPopover>
+        </div>
+        <div>
+            <label>
+                No Title
+                <es-button
+                    id="noTitleTarget"
+                    class="p-0 text-gray-700 align-text-bottom"
+                    tabindex="0"
+                    variant="link">
+                    <IconInfo
+                        width="16px"
+                        height="16px" />
+                </es-button>
+            </label>
+            <EsPopover target="noTitleTarget">
+                <p>
+                    Install solar panels through this program and get $250 cash back.
+                    <a
+                        class="mt-3 d-block"
+                        href="https://communitysolar.energysage.com/"
+                        target="_blank">Learn more.</a>
+                </p>
+            </EsPopover>
+        </div>
     </div>
 </template>
 <script>
