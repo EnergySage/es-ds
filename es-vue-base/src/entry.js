aaabd4fe11da0f2cd56a8a2e4f034f4cd468ef4a
Ryan Mulready <ryan.mulready@gmail.com>
2022-08-11 13:39:07 -0400

    Check if componentName is defined during exporting

--- es-vue-base/src/entry.js
+++ es-vue-base/src/entry.js
@@ -1,14 +1,16 @@
 // iife/cjs usage extends esm default export - so import it all
 // TODO: How to make this work for utils
-import plugin, * as components from '@/src/entry.esm';
+import plugin, * as exports from '@/src/entry.esm';
 
 // Attach named exports directly to plugin. IIFE/CJS will
 // only expose one global var, with component exports exposed as properties of
 // that global var (eg. plugin.component)
-Object.entries(components).forEach(([componentName, component]) => {
-    if (componentName !== 'default') {
+Object.entries(exports).forEach(([componentName, component]) => {
+    if (componentName && componentName !== 'default') {
         plugin[componentName] = component;
     }
 });
 
 export default plugin;
+
+export * from '@/src/lib-utils/index';
