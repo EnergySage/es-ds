54af9a32b014e2665bb4c16ef8e5612c0ebee539
Tommy <tomjleo@gmail.com>
2022-10-25 15:15:57 -0400

    Add extra-context slot

--- es-design-system/pages/molecules/es-form-input.vue
+++ es-design-system/pages/molecules/es-form-input.vue
@@ -1,26 +1,53 @@
 <template>
-    <b-row>
-        <b-col
-            cols="12"
-            lg="6">
-            <es-form-input
-                id="myId"
-                v-model="docInput">
-                <template #label>
-                    Account Number
-                </template>
-                <template #message>
-                    Please enter your account number.
-                </template>
-                <template #errorMessage>
-                    Please enter a valid account number.
-                </template>
-                <template #successMessage>
-                    Your account number has been submitted successfully.
-                </template>
-            </es-form-input>
-        </b-col>
-    </b-row>
+    <div>
+        <b-row>
+            <b-col
+                cols="12"
+                lg="6">
+                <es-form-input
+                    id="myId"
+                    v-model="docInput">
+                    <template #label>
+                        Account Number
+                    </template>
+                    <template #message>
+                        Please enter your account number.
+                    </template>
+                    <template #errorMessage>
+                        Please enter a valid account number.
+                    </template>
+                    <template #successMessage>
+                        Your account number has been submitted successfully.
+                    </template>
+                </es-form-input>
+            </b-col>
+        </b-row>
+        <b-row class="mt-4">
+            <b-col
+                cols="12"
+                lg="6">
+                <es-form-input
+                    id="myId2"
+                    v-model="example2">
+                    <template #label>
+                        Account Number
+                    </template>
+                    <template #extraContext>
+                        Extra Context Goes Here
+                    </template>
+                    <template #message>
+                        Please enter your account number.
+                    </template>
+                    <template #errorMessage>
+                        Please enter a valid account number.
+                    </template>
+                    <template #successMessage>
+                        Your account number has been submitted successfully.
+                    </template>
+                </es-form-input>
+            </b-col>
+        </b-row>
+    </div>
 </template>
 <script>
 // TODO: Add input state/msg validation example
@@ -34,6 +61,7 @@ export default {
     data() {
         return {
             docInput: 'Example text',
+            example2: 'Example two',
         };
     },
 };
