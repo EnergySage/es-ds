92081370d5ac494a2b6c86247b5ef27c93fd8e07
Tommy <tomjleo@gmail.com>
2022-11-09 14:53:11 -0500

    Add constants for referencing exposed vuelidate functions

--- es-vue-base/src/lib-validators/index.js
+++ es-vue-base/src/lib-validators/index.js
@@ -9,7 +9,7 @@ import {
     helpers,
 } from 'vuelidate/lib/validators';
 
-// Only some vuelidate validators can be reused; others dont work well
+// Only some vuelidate validators can be reused; others don't work well
 export const vuelidateRequired = required;
 export const vuelidateMinValue = minValue;
 export const vuelidateMaxValue = maxValue;
@@ -55,3 +55,34 @@ export function vuelidateHasUppercaseLetter(param) {
 export function vuelidateHasLowercaseLetter(param) {
     return !helpers.req(param) || hasLowercaseLetter(param);
 }
+
+const REQUIRED = 'vuelidateRequired';
+const MIN_VALUE = 'vuelidateMinValue';
+const MAX_VALUE = 'vuelidateMaxValue';
+const REQUIRED_IF = 'vuelidateRequiredIf';
+const NUMERIC = 'vuelidateNumeric';
+const MIN_LENGTH = 'vuelidateMinLength';
+const MAX_LENGTH = 'vuelidateMaxLength';
+const HAS_NUMBER = 'vuelidateHasNumber';
+const HAS_SPECIAL_CHARACTER = 'vuelidateHasSpecialCharacter';
+const HAS_UPPERCASE_LETTER = 'vuelidateHasUppercaseLetter';
+const HAS_LOWERCASE_LETTER = 'vuelidateHasLowercaseLetter';
+const EMAIL = 'vuelidateEmail';
+const PHONE = 'vuelidatePhone';
+
+export const vuelidateKeys = {
+    REQUIRED,
+    REQUIRED_IF,
+    MIN_VALUE,
+    MAX_VALUE,
+    MIN_LENGTH,
+    MAX_LENGTH,
+    NUMERIC,
+    EMAIL,
+    PHONE,
+    HAS_NUMBER,
+    HAS_SPECIAL_CHARACTER,
+    HAS_UPPERCASE_LETTER,
+    HAS_LOWERCASE_LETTER,
+
+};
