f63f4a7d9c27db5bd424b07028bc37cecc8fb26e
Ryan Mulready <ryan.mulready@gmail.com>
2023-02-24 18:27:48 -0500

    test(nuxt): move peers deps

--- es-vue-base/build/rollup.config.js
+++ es-vue-base/build/rollup.config.js
@@ -88,7 +88,6 @@ const external = [
     // list external dependencies, exactly the way it is written in the import statement.
     // eg. 'jquery'
     'vue',
-    'bootstrap-vue',
 ];
 
 // Refer to https://rollupjs.org/guide/en#output-globals for details
@@ -96,7 +95,6 @@ const globals = {
     // Provide global variable names to replace your external imports
     // eg. jquery: '$'
     vue: 'Vue',
-    'bootstrap-vue': 'bootstrapVue',
 };
 
 // UMD Build: https://github.com/umdjs/umd
