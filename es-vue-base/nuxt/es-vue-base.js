4f55b76b7f12a7fba72fc50d09d72f5ef691db2f
Ryan Mulready <ryan.mulready@gmail.com>
2023-02-27 09:35:19 -0500

    fix(rollup): remove bootstrap-vue as peer dep in es-vue-base

--- es-vue-base/nuxt/es-vue-base.js
+++ es-vue-base/nuxt/es-vue-base.js
@@ -2,19 +2,4 @@ import Vue from 'vue';
 // eslint-disable-next-line import/no-unresolved
 import install from '@energysage/es-vue-base';
 
-export default function autoExport(customOptions) {
-    const options = {
-        components: true,
-        icons: true,
-        ...customOptions,
-    };
-
-    /**
-     *  Auto import all components
-     *  The resulting build will include bundles for all possible components.
-     *  However those bundles will only be loaded if a component is needed
-     */
-    if (typeof options.components === 'boolean' && options.components) {
-        install(Vue);
-    }
-}
+install(Vue);
