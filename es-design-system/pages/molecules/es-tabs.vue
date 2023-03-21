e6b57230297d79fd28e989e5d9c4bc34e261d8a8
ericdouglaspratt <eric.pratt@energysage.com>
2023-03-21 10:49:26 -0400

    docs: updated EsTabs page to show programmatic examples

--- es-design-system/pages/molecules/es-tabs.vue
+++ es-design-system/pages/molecules/es-tabs.vue
@@ -3,28 +3,130 @@
         <h1>
             Tabs
         </h1>
-        <p>
+        <p class="mb-450">
             Extended from <b-link href="https://bootstrap-vue.org/docs/components/tabs">
                 bootstrap-vue tabs
             </b-link>
         </p>
-        <es-tabs class="my-450">
-            <es-tab title="Tab 1">
-                <p>
-                    Content 1
-                </p>
-            </es-tab>
-            <es-tab title="Tab 2">
-                <p>
-                    Content 2
-                </p>
-            </es-tab>
-            <es-tab title="Tab 3">
-                <p>
-                    Content 3
-                </p>
-            </es-tab>
-        </es-tabs>
+
+        <div class="mb-450">
+            <h2>
+                Basic example
+            </h2>
+            <es-tabs>
+                <es-tab title="Tab 1">
+                    <p>
+                        Content 1
+                    </p>
+                </es-tab>
+                <es-tab title="Tab 2">
+                    <p>
+                        Content 2
+                    </p>
+                </es-tab>
+                <es-tab title="Tab 3">
+                    <p>
+                        Content 3
+                    </p>
+                </es-tab>
+            </es-tabs>
+        </div>
+
+        <div class="mb-450">
+            <h2>
+                Selecting the active tab programmatically
+            </h2>
+            <es-tabs
+                v-model="tabIndexButtons"
+                class="mb-200">
+                <es-tab title="Tab 1">
+                    <p>
+                        Content 1
+                    </p>
+                </es-tab>
+                <es-tab title="Tab 2">
+                    <p>
+                        Content 2
+                    </p>
+                </es-tab>
+                <es-tab title="Tab 3">
+                    <p>
+                        Content 3
+                    </p>
+                </es-tab>
+            </es-tabs>
+            <div class="mb-100">
+                <es-button
+                    class="mb-50"
+                    size="sm"
+                    @click="tabIndexButtons = 0">
+                    Select tab 1
+                </es-button>
+                <es-button
+                    class="mb-50"
+                    size="sm"
+                    @click="tabIndexButtons = 1">
+                    Select tab 2
+                </es-button>
+                <es-button
+                    class="mb-50"
+                    size="sm"
+                    @click="tabIndexButtons = 2">
+                    Select tab 3
+                </es-button>
+            </div>
+        </div>
+
+        <div class="mb-450">
+            <h2>
+                Showing separate content based on the active tab
+            </h2>
+            <b-row>
+                <b-col
+                    cols="12"
+                    lg="6">
+                    <es-tabs
+                        v-model="tabIndexContent"
+                        class="mb-200">
+                        <es-tab title="Tab 1">
+                            <p>
+                                Content 1
+                            </p>
+                        </es-tab>
+                        <es-tab title="Tab 2">
+                            <p>
+                                Content 2
+                            </p>
+                        </es-tab>
+                        <es-tab title="Tab 3">
+                            <p>
+                                Content 3
+                            </p>
+                        </es-tab>
+                    </es-tabs>
+                </b-col>
+                <b-col
+                    cols="12"
+                    lg="6">
+                    <div
+                        v-if="tabIndexContent === 0"
+                        class="bg-gray-300 p-200 rounded-lg text-center">
+                        Content associated with tab 1
+                    </div>
+                    <div
+                        v-if="tabIndexContent === 1"
+                        class="bg-gray-300 p-200 rounded-lg text-center">
+                        Content associated with tab 2
+                    </div>
+                    <div
+                        v-if="tabIndexContent === 2"
+                        class="bg-gray-300 p-200 rounded-lg text-center">
+                        Content associated with tab 3
+                    </div>
+                </b-col>
+            </b-row>
+        </div>
+
         <ds-doc-source
             :comp-code="compCode"
             comp-source="es-vue-base/src/lib-components/EsTabs.vue"
@@ -38,6 +140,8 @@ export default {
     name: 'EsTabsDocs',
     data() {
         return {
+            tabIndexButtons: 0,
+            tabIndexContent: 0,
             compCode: '',
             docCode: '',
         };
