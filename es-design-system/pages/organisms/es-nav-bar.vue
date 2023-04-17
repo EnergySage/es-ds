478d1a7612c113198a5ea357fef0b83d559ab6e2
ericdouglaspratt <eric.pratt@energysage.com>
2023-04-17 14:26:28 -0400

    fix: allow rip-the-nav script to access the nav's JS

--- es-design-system/pages/organisms/es-nav-bar.vue
+++ es-design-system/pages/organisms/es-nav-bar.vue
@@ -13,6 +13,13 @@
 
         <es-nav-bar />
 
+        <!-- print out EsNavBar's mounted() script here so the rip-the-nav utility can access it -->
+        <!-- eslint-disable vue/no-v-html -->
+        <pre
+            class="d-none"
+            v-html="navScriptSource" />
+        <!-- eslint-enable vue/no-v-html -->
+
         <ds-doc-source
             :comp-code="compCode"
             comp-source="es-vue-base/src/lib-components/EsNavBar.vue"
@@ -24,6 +31,20 @@
 
 export default {
     name: 'EsNavBarDocs',
+    async asyncData() {
+        /* eslint-disable import/no-webpack-loader-syntax, import/no-self-import */
+        const compSource = await import('!raw-loader!@energysage/es-vue-base/src/lib-components/EsNavBar.vue');
+        /* eslint-enable import/no-webpack-loader-syntax, import/no-self-import */
+
+        // retrieve EsNavBar's mounted() script here so the rip-the-nav utility can access it
+        const compSourceText = compSource.default;
+        const scriptRegex = /\/\/ CUSTOM GLOBAL-NAV SCRIPT STARTS([\s\S]+)\/\/ CUSTOM GLOBAL-NAV SCRIPT ENDS/;
+        const navScriptSource = compSourceText.match(scriptRegex)[0];
+
+        return {
+            navScriptSource,
+        };
+    },
     data() {
         return {
             compCode: '',
