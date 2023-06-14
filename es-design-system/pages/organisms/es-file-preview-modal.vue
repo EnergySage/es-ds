51ea60e68b1304629384157a32f10290b6b94514
gigiwargin <gigi.wargin@energysage.com>
2023-06-14 11:41:47 -0400

    Documentation update

--- es-design-system/pages/organisms/es-file-preview-modal.vue
+++ es-design-system/pages/organisms/es-file-preview-modal.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="h-100">
-        <es-button @click="visible=true">
+        <es-button @click="visible = true">
             Show preview modal
         </es-button>
         <es-file-preview-modal
             file-source="https://www.eversource.com/content/images/default-source/bills/ct-sample-bill-electric-back.jpg?sfvrsn=e75bd262_4"
             file-name="bill.png"
-            :visible="visible">
+            :visible="visible"
+            @hidden="visible=false">
             <template #desktop-header>
                 Preview your image uploads.
                 <div class="font-size-base font-weight-normal pt-3">
