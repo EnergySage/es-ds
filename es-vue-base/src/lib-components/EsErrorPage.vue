096a23e8fdaa5f32219bbd89386c2c216c8c5558
suleiy <sulei.yadanar@energysage.com>
2023-06-26 14:46:38 -0400

    Switch <p> to <h2> for error message display

--- es-vue-base/src/lib-components/EsErrorPage.vue
+++ es-vue-base/src/lib-components/EsErrorPage.vue
@@ -1,32 +1,33 @@
 <template>
     <b-container class="my-450">
-        <b-col
-            md="7"
-            class="my-450 mx-auto text-center">
-            <h1 class="font-weight-boldest display-3">
-                {{ errorType }}
-            </h1>
-            <p
-                v-if="checkValidError"
-                id="msgError">
-                {{ errorCode[error] }}
-            </p>
-            <p
-                v-else
-                id="msgGeneric">
-                {{ genericMsg }}
-            </p>
-        </b-col>
-
         <b-row>
             <b-col
-                v-for="(icon, index) in iconList"
+                md="7"
+                class="my-450 mx-auto text-center">
+                <h1 class="font-weight-boldest display-3">
+                    {{ errorType }}
+                </h1>
+                <h2
+                    v-if="checkValidError"
+                    id="msgError">
+                    {{ errorCode[errorType] }}
+                </h2>
+                <h2
+                    v-else
+                    id="msgGeneric">
+                    {{ genericMsg }}
+                </h2>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col
+                v-for="(icon, index) in linkItems.icons"
                 :key="index"
                 cols="6"
                 md="3">
                 <a
                     class="align-items-center d-flex flex-column justify-content-center text-center"
-                    :href="linkList[index]">
+                    :href="linkItems.links[index]">
                     <div
                         class="bg-white d-flex justify-content-center mb-50 rounded-circle"
                         align-v="center">
@@ -38,7 +39,7 @@
                                 width="46px" />
                         </div>
                     </div>
-                    <p class="'font-weight-bolder mb-200 mb-md-0'">{{ labelList[index] }}</p>
+                    <p class="'font-weight-bolder mb-200 mb-md-0'">{{ linkItems.labels[index] }}</p>
                 </a>
             </b-col>
         </b-row>
@@ -68,26 +69,27 @@ export default {
     data() {
         return {
             errorCode: {
-                // eslint-disable-next-line max-len
-                err403: "Oops! If you're seeing this message, there's a good chance you have cookies or referrers turned off in your browser.",
-                err404: 'Oops! We are terribly sorry, but there is nothing bright to see here.',
-                err500: 'Oops! We are terribly sorry, but our server is not so bright today. Please try again.',
-                err503: 'Service unavailable: The page you are requesting is temporarily unavailable.',
+                // eslint-disable-next-line max-len, quote-props
+                '403': "Oops! If you're seeing this message, there's a good chance you have cookies or referrers turned off in your browser.",
+                // eslint-disable-next-line quote-props
+                '404': 'Oops! We are terribly sorry, but there is nothing bright to see here.',
+                // eslint-disable-next-line quote-props
+                '500': 'Oops! We are terribly sorry, but our server is not so bright today. Please try again.',
+                // eslint-disable-next-line quote-props
+                '503': 'Service unavailable: The page you are requesting is temporarily unavailable.',
             },
             genericMsg: 'Oops! Something went wrong!',
-            iconList: ['IconContactUs', 'IconCommunitySolar', 'IconInfoBlogPost', 'IconMarketplace'],
-            linkList: ['mailto:hello@energysage.com', 'https://www.energysage.com/solar/', 'https://news.energysage.com/', 'https://www.energysage.com/market-intro/'],
-            labelList: ['Contact Us', 'Learn About Solar', 'Informative Blog Post', 'EnergySage Marketplace'],
+            linkItems: {
+                icons: ['IconContactUs', 'IconCommunitySolar', 'IconInfoBlogPost', 'IconMarketplace'],
+                links: ['mailto:hello@energysage.com', 'https://www.energysage.com/solar/', 'https://news.energysage.com/', 'https://www.energysage.com/market-intro/'],
+                labels: ['Contact Us', 'Learn About Solar', 'Informative Blog Post', 'EnergySage Marketplace'],
+            },
         };
     },
     computed: {
-        error() {
-            return `err${this.errorType}`;
-        },
         checkValidError() {
-            return Object.keys(this.errorCode).includes(this.error);
+            return Object.keys(this.errorCode).includes(this.errorType);
         },
     },
-
 };
 </script>
