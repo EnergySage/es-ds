42ef81149b11c89ff9515d15305b34ef15cc39c9
ericdouglaspratt <eric.pratt@energysage.com>
2023-07-26 08:19:35 -0400

    fix: removed unnecessary div

--- es-vue-base/src/lib-components/EsCtaBanner.vue
+++ es-vue-base/src/lib-components/EsCtaBanner.vue
@@ -1,38 +1,36 @@
 <template>
-    <div>
-        <es-card
-            class="px-100 px-lg-300"
-            :class="[{ 'bg-gray text-white': dark }]"
-            v-bind="$attrs"
-            v-on="$listeners">
-            <b-row>
-                <b-col
-                    class="mb-200 my-lg-auto text-center text-lg-left"
-                    :lg="lgFirst">
-                    <h2
-                        :class="{
-                            'mb-50': $slots.subtitle,
-                            'mb-0': !$slots.subtitle,
-                            'text-white': dark,
-                        }">
-                        <slot name="heading">
-                            Easily find what solar costs in your area
-                        </slot>
-                    </h2>
-                    <p
-                        v-if="$slots.subtitle"
-                        class="mb-0">
-                        <slot name="subtitle" />
-                    </p>
-                </b-col>
-                <b-col
-                    class="d-flex justify-content-center justify-content-lg-end my-auto"
-                    :lg="lgSecond">
-                    <slot name="cta" />
-                </b-col>
-            </b-row>
-        </es-card>
-    </div>
+    <es-card
+        class="px-100 px-lg-300"
+        :class="[{ 'bg-gray text-white': dark }]"
+        v-bind="$attrs"
+        v-on="$listeners">
+        <b-row>
+            <b-col
+                class="mb-200 my-lg-auto text-center text-lg-left"
+                :lg="lgFirst">
+                <h2
+                    :class="{
+                        'mb-50': $slots.subtitle,
+                        'mb-0': !$slots.subtitle,
+                        'text-white': dark,
+                    }">
+                    <slot name="heading">
+                        Easily find what solar costs in your area
+                    </slot>
+                </h2>
+                <p
+                    v-if="$slots.subtitle"
+                    class="mb-0">
+                    <slot name="subtitle" />
+                </p>
+            </b-col>
+            <b-col
+                class="d-flex justify-content-center justify-content-lg-end my-auto"
+                :lg="lgSecond">
+                <slot name="cta" />
+            </b-col>
+        </b-row>
+    </es-card>
 </template>
 
 <script lang="js">
