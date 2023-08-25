96b1c4108153d80bbef7f5556aa28682ad06c810
Nathaniel MacArthur-Warner <nathaniel.macarthur-warner@energysage.com>
2023-08-25 16:30:33 -0400

    fix: avoid flashing entirely with inline script that creates style tag

--- es-vue-base/src/lib-components/EsFixedBanner.vue
+++ es-vue-base/src/lib-components/EsFixedBanner.vue
@@ -1,13 +1,6 @@
 <template>
     <div>
         <div
-            id="singleEsFixedBanner"
-            ref="banner"
-            class="fixed-bottom bg-white"
-            :class="{'d-none': bannerShouldBeHidden }">
-            CTA text will go here
-        </div>
-        <div
             v-html="`<script>
 function getCookieValue(cookieName) {
     const cookies = document.cookie.split('; ');
@@ -19,37 +12,24 @@ function getCookieValue(cookieName) {
     }
     return null;
 }
-console.log('howwdy');
 const bannerShouldBeHidden = getCookieValue('bannerShouldBeHidden');
 if (bannerShouldBeHidden) {
-    const banner = document.getElementById('singleEsFixedBanner');
-    console.log(banner.classList);
-    banner.classList.add('d-none');
-    console.log(banner.classList);
+    const style = document.createElement('style');
+    document.head.appendChild(style);
+    style.sheet.insertRule('.EsFixedBanner { display: none !important }');
 }
 </script>`" />
+        <div
+            id="singleEsFixedBanner"
+            ref="banner"
+            class="fixed-bottom bg-white EsFixedBanner">
+            CTA text will go here
+        </div>
     </div>
 </template>
 
 <script lang="js">
-function getCookieValue(cookieName) {
-    const cookies = document.cookie.split('; ');
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].split('=');
-        if (cookie[0] === cookieName) {
-            return decodeURIComponent(cookie[1]);
-        }
-    }
-    return null;
-}
-
 export default {
     name: 'EsFixedBanner',
-    data() {
-        const bannerShouldBeHidden = typeof document !== 'undefined' && !!getCookieValue('bannerShouldBeHidden');
-        return {
-            bannerShouldBeHidden,
-        };
-    },
 };
 </script>
