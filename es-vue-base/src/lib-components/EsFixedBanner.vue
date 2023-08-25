1384049efc2f238975be9a0552e2c307890ed4aa
Nathaniel MacArthur-Warner <nathaniel.macarthur-warner@energysage.com>
2023-08-25 12:12:05 -0400

    fix: use vue way of doing things to get it working

--- es-vue-base/src/lib-components/EsFixedBanner.vue
+++ es-vue-base/src/lib-components/EsFixedBanner.vue
@@ -1,34 +1,35 @@
 <template>
-    <div>
-        <div
-            id="singleEsFixedBanner"
-            class="fixed-bottom bg-white d-none"
-            ref="banner">
-            CTA text will go here
-        </div>
-        <div
-v-html="`<script>function getCookieValue(cookieName) {
-const cookies = document.cookie.split('; ');
-for (let i = 0; i < cookies.length; i++) {
-const cookie = cookies[i].split('=');
-if (cookie[0] === cookieName) {
-return decodeURIComponent(cookie[1]);
-}
-}
-return null;
-}
-console.log('howdy');
-const bannerShouldBeHidden = getCookieValue('esFixedBannerShouldBeHidden');
-if (!bannerShouldBeHidden) {
-const banner = document.getElementById('singleEsFixedBanner');
-banner.classList.remove('d-none');
-}</script>`">
-        </div>
+    <div
+        id="singleEsFixedBanner"
+        ref="fixedBanner"
+        class="fixed-bottom bg-white"
+        :class="{'d-none': bannerShouldBeHidden}">
+        CTA text will go here
     </div>
 </template>
 
 <script lang="js">
+function getCookieValue(cookieName) {
+    const cookies = document.cookie.split('; ');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].split('=');
+        if (cookie[0] === cookieName) {
+            return decodeURIComponent(cookie[1]);
+        }
+    }
+    return null;
+}
+
 export default {
     name: 'EsFixedBanner',
+    data() {
+        return {
+            bannerShouldBeHidden: false,
+        };
+    },
+    mounted() {
+        const cookieValue = getCookieValue('bannerShouldBeHidden');
+        this.bannerShouldBeHidden = !!cookieValue;
+    },
 };
 </script>
