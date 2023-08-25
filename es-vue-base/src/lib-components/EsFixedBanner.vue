11953e94d2d36cf75aa3c2b5e365d23993778ed2
Nathaniel MacArthur-Warner <nathaniel.macarthur-warner@energysage.com>
2023-08-25 16:07:53 -0400

    fix: attempt to get cookie reading without flicker, mostly successful

--- es-vue-base/src/lib-components/EsFixedBanner.vue
+++ es-vue-base/src/lib-components/EsFixedBanner.vue
@@ -2,33 +2,54 @@
     <div>
         <div
             id="singleEsFixedBanner"
-            class="fixed-bottom bg-white d-none"
-            ref="banner">
+            ref="banner"
+            class="fixed-bottom bg-white"
+            :class="{'d-none': bannerShouldBeHidden }">
             CTA text will go here
         </div>
         <div
-v-html="`<script>function getCookieValue(cookieName) {
-const cookies = document.cookie.split('; ');
-for (let i = 0; i < cookies.length; i++) {
-const cookie = cookies[i].split('=');
-if (cookie[0] === cookieName) {
-return decodeURIComponent(cookie[1]);
+            v-html="`<script>
+function getCookieValue(cookieName) {
+    const cookies = document.cookie.split('; ');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].split('=');
+        if (cookie[0] === cookieName) {
+            return decodeURIComponent(cookie[1]);
+        }
+    }
+    return null;
 }
+console.log('howwdy');
+const bannerShouldBeHidden = getCookieValue('bannerShouldBeHidden');
+if (bannerShouldBeHidden) {
+    const banner = document.getElementById('singleEsFixedBanner');
+    console.log(banner.classList);
+    banner.classList.add('d-none');
+    console.log(banner.classList);
 }
-return null;
-}
-console.log('howdy');
-const bannerShouldBeHidden = getCookieValue('esFixedBannerShouldBeHidden');
-if (!bannerShouldBeHidden) {
-const banner = document.getElementById('singleEsFixedBanner');
-banner.classList.remove('d-none');
-}</script>`">
-        </div>
+</script>`" />
     </div>
 </template>
 
 <script lang="js">
+function getCookieValue(cookieName) {
+    const cookies = document.cookie.split('; ');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].split('=');
+        if (cookie[0] === cookieName) {
+            return decodeURIComponent(cookie[1]);
+        }
+    }
+    return null;
+}
+
 export default {
     name: 'EsFixedBanner',
+    data() {
+        const bannerShouldBeHidden = typeof document !== 'undefined' && !!getCookieValue('bannerShouldBeHidden');
+        return {
+            bannerShouldBeHidden,
+        };
+    },
 };
 </script>
