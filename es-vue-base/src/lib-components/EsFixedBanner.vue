0788488b15cc333ce4d4e4278b4f90c5a720fc9d
Nathaniel MacArthur-Warner <nathaniel.macarthur-warner@energysage.com>
2023-08-25 10:08:02 -0400

    feat: progress with a better workaround for inline script

--- es-vue-base/src/lib-components/EsFixedBanner.vue
+++ es-vue-base/src/lib-components/EsFixedBanner.vue
@@ -2,14 +2,28 @@
     <div>
         <div
             id="singleEsFixedBanner"
-            class="fixed-bottom bg-white d-none">
-            howdyy
+            class="fixed-bottom bg-white d-none"
+            ref="banner">
+            CTA text will go here
         </div>
-        <component is="script">
-            const banner = document.getElementById('singleEsFixedBanner');
-            console.log(banner);
-            banner.classList.remove('d-none');
-        </component>
+        <div
+v-html="`<script>function getCookieValue(cookieName) {
+const cookies = document.cookie.split('; ');
+for (let i = 0; i < cookies.length; i++) {
+const cookie = cookies[i].split('=');
+if (cookie[0] === cookieName) {
+return decodeURIComponent(cookie[1]);
+}
+}
+return null;
+}
+console.log('howdy');
+const bannerShouldBeHidden = getCookieValue('esFixedBannerShouldBeHidden');
+if (!bannerShouldBeHidden) {
+const banner = document.getElementById('singleEsFixedBanner');
+banner.classList.remove('d-none');
+}</script>`">
+        </div>
     </div>
 </template>
 
