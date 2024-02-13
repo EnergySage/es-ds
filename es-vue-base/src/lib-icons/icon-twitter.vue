69cd622519d862c76469f99ad5174ba408f306d4
Jaissica Hora <jaissicahora@Jaissicas-Laptop.local>
2024-02-13 09:56:52 -0500

    Updated dimensions of X logo

--- es-vue-base/src/lib-icons/icon-twitter.vue
+++ es-vue-base/src/lib-icons/icon-twitter.vue
@@ -1,5 +1,5 @@
 <template>
-     <svg
+  <svg
     :style="{
       height: height,
       width: width,
@@ -11,34 +11,34 @@
     <path fill="none" d="M0 0.4h31v31H0z" />
     <polygon
       fill="white"
-      points="16.18 14.46 11.22 7.58 8.58 7.58 14.73 16.11 15.5 17.18 15.5 17.18 20.76 24.48 23.41 24.48 16.96 15.53 16.18 14.46"
+      points="15.67 14.46 10.87 7.58 8.31 7.58 14.27 16.11 15.02 17.18 15.02 17.18 20.11 24.48 22.68 24.48 16.43 15.53 15.67 14.46"
     />
     <path
       fill="currentColor"
-      d="M16 0C7.16 0 0 7.16 0 16s7.16 16 16 16 16-7.16 16-16S24.84 0 16 0ZM19.95 25.67l-5.33-7.51-6.67 7.51h-1.72l7.63-8.59L6.23 6.33h5.82l5.05 7.12 6.32-7.12h1.72l-7.28 8.19h0l7.91 11.15h-5.82Z"
+      d="M15.5 0C6.94 0 0 7.16 0 16s6.94 16 15.5 16 15.5-7.16 15.5-16S24.06 0 15.5 0ZM19.33 25.67l-5.16-7.51-6.46 7.51h-1.67l7.39-8.59L6.04 6.33h5.64l4.89 7.12 6.12-7.12h1.67l-7.05 8.19h0l7.66 11.15s-5.64 0-5.64 0Z"
     />
   </svg>
 </template>
 <script>
 export default {
-    name: 'IconTwitter',
-    props: {
-        /**
-         * Width
-         */
-        width: {
-            type: String,
-            default: '31px',
-            required: false,
-        },
-        /**
-         * Height
-         */
-        height: {
-            type: String,
-            default: '32px',
-            required: false,
-        },
+  name: 'IconTwitter',
+  props: {
+    /**
+     * Width
+     */
+    width: {
+      type: String,
+      default: '31px',
+      required: false,
     },
+    /**
+     * Height
+     */
+    height: {
+      type: String,
+      default: '32px',
+      required: false,
+    },
+  },
 };
 </script>
