7b91243c4af18b556de6ecaef0e9a318458ad166
RANE1021 <rafael.nunez@energysage.com>
2024-05-01 15:02:32 -0400

    feat: use new url for design v2

--- es-design-system/aws/bin/aws.js
+++ es-design-system/aws/bin/aws.js
@@ -7,7 +7,7 @@ const app = new cdk.App();
 
 // eslint-disable-next-line no-new
 new NuxtStaticStack(app, 'DesignSystemStack', {
-    stackName: 'es-design-system',
+    stackName: 'es-design-system-2',
     env: {
         // The ID of your AWS account on which to deploy the stack.
         account: '453321834875',
