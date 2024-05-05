03c20bf5d55a9430d375454ef6714af58ee4f454
ericdouglaspratt <eric.pratt@energysage.com>
2024-05-05 11:47:28 -0400

    feat: interactive cards now automatically become links or buttons, updated EsCard and EsVideo styling

--- es-vue-base/src/lib-components/EsVideo.vue
+++ es-vue-base/src/lib-components/EsVideo.vue
@@ -7,9 +7,10 @@
             :src="autoplayUrl"
             :title="altText"
             type="iframe" />
-        <es-button
+        <es-card
             v-else
-            class="EsVideo-button text-gray-900 border-0 p-0 position-relative text-gray w-100 bg-transparent"
+            class="EsVideo-button bg-transparent overflow-hidden p-0 position-relative w-100"
+            variant="interactive"
             @click="showVideo = true">
             <slot
                 v-if="hasImage"
@@ -18,13 +19,14 @@
                 v-else-if="coverImageUrl"
                 :alt="altText"
                 sizes="md:530px sm:275px"
-                class="EsVideo-image d-block rounded-lg w-100"
+                class="EsVideo-image d-block w-100"
                 :src="coverImageUrl" />
             <icon-video-play
                 class="EsVideo-icon position-absolute"
                 width="74px"
                 height="54px" />
-        </es-button>
+            </es-button>
+        </es-card>
     </div>
 </template>
 
