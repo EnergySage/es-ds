f2983a09d454611d424bd5c205777c895738b2b6
Nathaniel MacArthur-Warner <nathaniel.macarthur-warner@energysage.com>
2024-08-12 12:03:28 -0400

    feat: implement v-model support for accordion

--- es-ds-components/components/es-accordion-list.vue
+++ es-ds-components/components/es-accordion-list.vue
@@ -14,18 +14,36 @@ const props = withDefaults(defineProps<Props>(), {
     variant: "default",
 });
 
+const model = defineModel();
+
 const children = useSlots().default?.() || [];
+
 let activeIndex;
+
+watch(model, (newVal, oldVal) => {
+    if (newVal) {
+        activeIndex = children.findIndex((child) => child.props.id === newVal);
+    }
+})
+
 const accordionTabs = children.map((child, index) => {
     if (child.props.id === props.initialExpandedId) {
+        activeIndex = index;
+    } else if (child.props.id === model.value) {
         activeIndex = index;
     }
     return child.children;
 });
+
+const updateActiveIndex = (index) => {
+    if (model.value) {
+        model.value = children[index].props.id;
+    }
+};
 </script>
 
 <template>
-    <accordion :multiple="allowMultipleExpand" :active-index="activeIndex">
+    <accordion :multiple="allowMultipleExpand" :active-index="activeIndex" @update:active-index="updateActiveIndex">
         <accordion-tab v-for="(tab, index) in accordionTabs" :key="index">
             <template #header>
                 <span>
