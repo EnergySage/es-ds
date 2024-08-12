58a118b01c511dbb3d727d2da75ee1543e2da006
Nathaniel MacArthur-Warner <nathaniel.macarthur-warner@energysage.com>
2024-08-12 17:38:09 -0400

    feat: styling and reliability improvements

--- es-ds-components/components/es-accordion-list.vue
+++ es-ds-components/components/es-accordion-list.vue
@@ -18,33 +18,57 @@ const model = defineModel();
 
 const children = useSlots().default?.() || [];
 
-let activeIndex;
+const activeIndex = ref();
 
 watch(model, (newVal, oldVal) => {
     if (newVal) {
-        activeIndex = children.findIndex((child) => child.props.id === newVal);
+        activeIndex.value = children.findIndex((child) => child.props.id === newVal);
     }
 })
 
 const accordionTabs = children.map((child, index) => {
     if (child.props.id === props.initialExpandedId) {
-        activeIndex = index;
+        activeIndex.value = index;
     } else if (child.props.id === model.value) {
-        activeIndex = index;
+        activeIndex.value = index;
     }
     return child.children;
 });
 
 const updateActiveIndex = (index) => {
     if (model.value) {
-        model.value = children[index].props.id;
+        model.value = children[index]?.props.id || '';
     }
 };
 </script>
 
 <template>
-    <accordion :multiple="allowMultipleExpand" :active-index="activeIndex" @update:active-index="updateActiveIndex">
-        <accordion-tab v-for="(tab, index) in accordionTabs" :key="index">
+    <accordion :multiple="allowMultipleExpand" :active-index="activeIndex" @update:active-index="updateActiveIndex" :pt="{
+        root: {
+            role: 'tablist',
+            class: 'es-accordion-list es-accordion-list--default',
+        },
+    }">
+        <accordion-tab v-for="(tab, index) in accordionTabs" :key="index" :pt="{
+            root: ({ context }) => ({
+                role: 'tab',
+                class: [
+                    'es-accordion es-accordion--default',
+                    {
+                        'expanded': context.active,
+                    },
+                ],
+            }),
+            header: {
+                class: 'position-relative d-block',
+            },
+            headerAction: {
+                class: 'es-accordion-heading mb-0 align-items-center d-flex font-weight-bold justify-content-between py-100 es-accordion-heading--visible font-size-100 px-100 px-sm-200',
+            },
+            toggleableContent: {
+                class: 'es-accordion-content pb-25 bg-white pt-100 px-100 px-sm-200',
+            },
+        }">
             <template #header>
                 <span>
                     <component v-for="item in tab.title()" :is="item" />
