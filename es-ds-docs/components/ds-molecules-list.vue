d0b7673b1ffc7754b014801b54faa65b256edb64
Michael LeRoux <mike@energysage.com>
2024-08-13 17:08:33 -0400

    feat: add EsBadge component

--- es-ds-docs/components/ds-molecules-list.vue
+++ es-ds-docs/components/ds-molecules-list.vue
@@ -1,14 +1,13 @@
 <template>
     <ul class="list-unstyled pl-100">
         <li>
-            <ds-link to="/molecules/breadcrumbs">
-                Breadcrumbs
-            </ds-link>
+            <ds-link to="/molecules/badge"> Badge </ds-link>
         </li>
         <li>
-            <ds-link to="/molecules/collapse">
-                Collapse
-            </ds-link>
+            <ds-link to="/molecules/breadcrumbs"> Breadcrumbs </ds-link>
+        </li>
+        <li>
+            <ds-link to="/molecules/collapse"> Collapse </ds-link>
         </li>
     </ul>
 </template>
---
+++ es-ds-docs/pages/molecules/badge.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Badge</h1>
+        <p>Extended from <a href="https://v3.primevue.org/badge/" target="_blank"> PrimeVue Badge </a></p>
+
+        <div class="my-500">
+            <es-badge> Primary </es-badge>
+
+            <es-badge class="ml-50" variant="success"> Success </es-badge>
+
+            <es-badge class="ml-50" variant="warning"> Warning </es-badge>
+
+            <es-badge class="ml-50" variant="danger"> Danger </es-badge>
+
+            <es-badge class="ml-50" variant="info"> Info </es-badge>
+
+            <es-badge class="ml-50" variant="dark"> Dark </es-badge>
+        </div>
+
+        <ds-doc-source
+            :comp-code="compCode"
+            comp-source="es-ds-components/components/es-badge.vue"
+            :doc-code="docCode"
+            doc-source="es-ds-docs/pages/molecules/badge.vue"
+        />
+    </div>
+</template>
+
+<script setup>
+const { $prism } = useNuxtApp();
+const compCode = ref("");
+const docCode = ref("");
+
+if ($prism) {
+    const compSource = await import("@energysage/es-ds-components/components/es-badge.vue?raw");
+    const docSource = await import("./badge.vue?raw");
+    compCode.value = $prism.normalizeCode(compSource.default);
+    docCode.value = $prism.normalizeCode(docSource.default);
+    $prism.highlight();
+}
+</script>
