92b394791b2100da3190fc873eec0ea1008872b4
Nathaniel MacArthur-Warner <nathaniel.macarthur-warner@energysage.com>
2024-08-16 15:56:53 -0400

    feat: implement auto hiding

--- es-ds-components/components/es-form-msg.vue
+++ es-ds-components/components/es-form-msg.vue
@@ -16,12 +16,26 @@ const props = defineProps({
         validator: (val: string) => ['danger', 'success', 'primary'].includes(val),
     },
 });
+
+const emit = defineEmits(['hidden']);
+
+const hide = () => {
+    emit('hidden');
+}
+
+watch(() => props.show, (newValue, oldValue) => {
+    if (newValue === true && oldValue === false) {
+        setTimeout(() => {
+            hide();
+        }, props.timeout * 1000)
+    }
+});
 </script>
 
 <template>
     <div v-if="show" role="alert" aria-live="polite" aria-atomic="true"
          class="alert es-form-msg my-100 alert-dismissible" :class="`alert-${variant}`">
-        <button type="button" aria-label="Close" class="close" @click="$emit('hidden')">
+        <button type="button" aria-label="Close" class="close" @click="hide">
             icon-x
         </button>
         <div class="d-flex pr-100">
