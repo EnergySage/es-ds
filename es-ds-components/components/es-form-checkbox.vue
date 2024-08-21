d2e38da05c7d0edda0b60ba796d1970aa8e1edc7
Tommy <tomjleo@gmail.com>
2024-08-21 10:03:02 -0400

    chore: work-in-progress

---
+++ es-ds-components/components/es-form-checkbox.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineOptions({
+  inheritAttrs: false
+})
+</script>
+
+<template>
+    <div
+        class="custom-control custom-checkbox">
+        <checkbox
+            :pt="{
+                root: 'custom-control-input'
+            }"
+            v-bind="$attrs"
+        />
+        <label class="custom-control-label">
+            <slot />
+        </label>
+    </div>
+</template>
---
+++ es-ds-docs/pages/molecules/checkbox.vue
@@ -0,0 +1,60 @@
+<script setup>
+// const docCode = ref('');
+const apple = ref(false);
+const banana = ref(true);
+const cherry = ref(false);
+const selectAll = ref(false);
+
+const selectAllIsIndeterminate = computed(() => {
+    return (this.apple && this.banana && this.cherry) !== (this.apple || this.banana || this.cherry);
+});
+
+const handleOptionChange = () => {
+    selectAll = apple && banana && cherry;
+}
+
+const handleSelectAllToggle = () => {
+    if (!(apple && banana && cherry)) {
+        apple = true;
+        banana = true;
+        cherry = true;
+    } else {
+        apple = false;
+        banana = false;
+        cherry = false;
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>
+            Checkbox
+        </h1>
+        <p class="mb-500">
+            Uses
+            <a
+                href="https://v3.primevue.org/checkbox/"
+                target="_blank">
+                primevue checkbox
+            </a>
+        </p>
+
+        <div class="my-500">
+            <h2>
+                Basic example
+            </h2>
+            <div>
+                <es-form-checkbox v-model="apple">
+                    Apple
+                </es-form-checkbox>
+                <es-form-checkbox v-model="banana">
+                    Banana
+                </es-form-checkbox>
+                <es-form-checkbox v-model="cherry">
+                    Cherry
+                </es-form-checkbox>
+            </div>
+        </div>
+    </div>
+</template>
