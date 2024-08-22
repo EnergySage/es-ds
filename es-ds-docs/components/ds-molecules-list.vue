3fe282e0e986922c92b406619628536100029ffe
Michael LeRoux <mike@energysage.com>
2024-08-22 16:22:13 -0400

    chore: restore original formatting

--- es-ds-docs/components/ds-molecules-list.vue
+++ es-ds-docs/components/ds-molecules-list.vue
@@ -1,40 +1,64 @@
 <template>
     <ul class="list-unstyled pl-100">
         <li>
-            <ds-link to="/molecules/accordion"> Accordion </ds-link>
+            <ds-link to="/molecules/accordion">
+                Accordion
+            </ds-link>
         </li>
         <li>
-            <ds-link to="/molecules/badge"> Badge </ds-link>
+            <ds-link to="/molecules/badge">
+                Badge
+            </ds-link>
         </li>
         <li>
-            <ds-link to="/molecules/breadcrumbs"> Breadcrumbs </ds-link>
+            <ds-link to="/molecules/breadcrumbs">
+                Breadcrumbs
+            </ds-link>
         </li>
         <li>
-            <ds-link to="/molecules/card"> Card </ds-link>
+            <ds-link to="/molecules/card">
+                Card
+            </ds-link>
         </li>
         <li>
-            <ds-link to="/molecules/collapse"> Collapse </ds-link>
+            <ds-link to="/molecules/collapse">
+                Collapse
+            </ds-link>
         </li>
         <li>
-            <ds-link to="/molecules/data-table"> Data table </ds-link>
+            <ds-link to="/molecules/data-table">
+                Data table
+            </ds-link>
+         </li>
+         <li>
+            <ds-link to="/molecules/data-table-simple">
+              Data table simple
+            </ds-link>
         </li>
         <li>
-            <ds-link to="/molecules/data-table-simple"> Data table simple </ds-link>
+            <ds-link to="/molecules/form-msg">
+                Form Message
+            </ds-link>
         </li>
         <li>
-            <ds-link to="/molecules/form-msg"> Form Message </ds-link>
+            <ds-link to="/molecules/modal">
+                Modal
+            </ds-link>
         </li>
         <li>
-            <ds-link to="/molecules/modal"> Modal </ds-link>
+            <ds-link to="/molecules/progress">
+                Progress
+            </ds-link>
         </li>
+         <li>
+            <ds-link to="/molecules/progress-circle">
+                Progress circle
+            </ds-link>
+         </li>
         <li>
-            <ds-link to="/molecules/progress"> Progress </ds-link>
-        </li>
-        <li>
-            <ds-link to="/molecules/progress-circle"> Progress circle </ds-link>
-        </li>
-        <li>
-            <ds-link to="/molecules/radio-button"> Radio button </ds-link>
+            <ds-link to="/molecules/radio-button">
+                Radio button
+            </ds-link>
         </li>
     </ul>
 </template>
