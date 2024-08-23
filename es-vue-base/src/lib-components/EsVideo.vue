8aac880c63f8d633fcae4c3e1a932a2a3bdc9c19
Jaissica Hora <jaissicahora@Jaissicas-Laptop.local>
2024-08-23 11:02:51 -0400

    fix: added EsLazyYoutube to play video on single click

--- es-vue-base/src/lib-components/EsVideo.vue
+++ es-vue-base/src/lib-components/EsVideo.vue
@@ -1,15 +1,21 @@
 <template>
-    <div>
-        <b-embed
+    <div class="EsLazyYoutube">
+        <es-card
             v-if="showVideo"
-            allowfullscreen
-            aspect="16by9"
-            :src="autoplayUrl"
-            :title="altText"
-            type="iframe" />
+            class="EsLazyYoutube-button bg-transparent overflow-hidden p-0 position-relative w-100"
+            variant="interactive">
+            <LazyYoutubeVideo
+                allowfullscreen
+                ref="lazyYoutube"
+                autoplay="true"
+                class="pb-0 EsLazyYoutube-restrict w-100 h-100"
+                :style="videoStyles"
+                :src="autoplayUrl"
+                :title="altText" />
+        </es-card>
         <es-card
             v-else
-            class="EsVideo-button bg-transparent overflow-hidden p-0 position-relative w-100"
+            class="EsLazyYoutube-button bg-transparent overflow-hidden p-0 position-relative w-100"
             variant="interactive"
             @click="showVideo = true">
             <slot
@@ -19,10 +25,10 @@
                 v-else-if="coverImageUrl"
                 :alt="altText"
                 sizes="md:530px sm:275px"
-                class="EsVideo-image d-block w-100"
+                class="EsLazyYoutube-image d-block w-100"
                 :src="coverImageUrl" />
             <icon-video-play
-                class="EsVideo-icon position-absolute"
+                class="EsLazyYoutube-icon position-absolute"
                 width="74px"
                 height="54px" />
         </es-card>
@@ -30,17 +36,19 @@
 </template>
 
 <script lang="js">
-import { BImg, BEmbed } from 'bootstrap-vue';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import LazyYoutubeVideo from 'vue-lazy-youtube-video';
+import { BImg } from 'bootstrap-vue';
 import EsCard from './EsCard.vue';
 import IconVideoPlay from '../lib-icons/icon-video-play.vue';
 
 export default ({
-    name: 'EsVideo',
+    name: 'EsLazyYoutube',
     components: {
         BImg,
-        BEmbed,
         EsCard,
         IconVideoPlay,
+        LazyYoutubeVideo,
     },
     props: {
         altText: {
@@ -72,22 +80,85 @@ export default ({
             // but requires that the video owner upload non-auto-generated captions
             return `${pieces[0]}?rel=0&autoplay=1&cc_load_policy=1&cc_lang_pref=en`;
         },
+        videoStyles() {
+            return {
+                position: 'relative',
+                paddingBottom: '0 !important',
+                height: '100%',
+                width: '100%',
+            };
+        },
     },
+    // eslint-disable-next-line max-len, no-param-reassign
+    watch: { showVideo(val) { if (val) { this.$nextTick(() => { const videoInner = this.$refs.lazyYoutube.$el.querySelector('.y-video__inner'); if (videoInner) { videoInner.style.paddingBottom = '0'; const observer = new MutationObserver((mutations) => { mutations.forEach((mutation) => { if (mutation.attributeName === 'style') { mutation.target.style.paddingBottom = '0'; } }); }); observer.observe(videoInner, { attributes: true }); } }); } } },
 });
 </script>
 
 <style lang="scss" scoped>
-.EsVideo {
-    &-button,
-    &-image {
-        height: auto;
+.EsLazyYoutube {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &-button {
+        height: 100%;
+        padding-bottom: 0 !important;
+        position: relative;
+        width: 100%;
     }
 
     &-icon {
         left: 50%;
+        padding-bottom: 0 !important;
         top: 50%;
         transform: translateX(-50%) translateY(-50%);
         z-index: 2;
     }
+    &-image,
+    &-restrict
+    &-iframe,
+    iframe
+    {
+        height: 100% !important;
+        left: 0 !important;
+        //object-fit: cover !important;
+        padding-bottom: 0 !important;
+        position: relative !important;
+        top: 0 !important;
+        width: 100% !important;
+    }
 }
+.y-video__inner {
+    height: 100% !important;
+    position: relative !important;
+    padding-bottom: 0 !important;
+    width: 100% !important;
+}
+
+.y-video__button {
+    padding-bottom: 0 !important;
+}
+
+.y-video__media {
+    //position: relative !important;
+    height: 800px !important;
+    padding-bottom: 0 !important;
+    width: 800px !important;
+}
+.movie_player {
+    padding-bottom: 0 !important;
+    //object-fit: cover !important;
+}
+
+.html5-video-player {
+    height: 800px !important;
+    padding-bottom: 0 !important;
+    width: 800px !important;
+}
+
+.movie_player {
+    height: 800px !important;
+    padding-bottom: 0 !important;
+    width: 800px !important;
+}
+
 </style>
