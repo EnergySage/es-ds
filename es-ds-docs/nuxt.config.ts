463548ee3c0b84c3209319150b4dc929d639b3ad
Tommy <tomjleo@gmail.com>
2024-08-28 14:04:32 -0400

    feat: fix vite version

--- es-ds-docs/nuxt.config.ts
+++ es-ds-docs/nuxt.config.ts
@@ -10,4 +10,27 @@ export default defineNuxtConfig({
 
     // https://nuxt.com/docs/getting-started/deployment#static-hosting
     ssr: true,
+
+    // Per suggestion from https://github.com/nuxt/nuxt/issues/26783#issuecomment-2058966800
+    // See also:
+    // - https://github.com/nuxt/nuxt/issues/24196#issuecomment-1853870504
+    vite: {
+        optimizeDeps: {
+            include: [
+                'primevue/rating',
+                'primevue/inputtext',
+                'primevue/inputmask',
+                'primevue/breadcrumb',
+                'primevue/panel',
+                'primevue/radiobutton',
+                'primevue/accordion',
+                'primevue/accordiontab',
+                'primevue/datatable',
+                'primevue/column',
+                'primevue/dialog',
+                'primevue/button',
+                'primevue/progressbar',
+            ]
+        }
+    }
 })
