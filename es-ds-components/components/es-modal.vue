b5fb40a1adb9e3a073a25fc4126d780098f632c6
Michael LeRoux <mike@energysage.com>
2024-08-29 11:57:20 -0400

    fix: remove first attempt at mask/transition

--- es-ds-components/components/es-modal.vue
+++ es-ds-components/components/es-modal.vue
@@ -17,9 +17,6 @@ const modalPt = {
     root: {
         class: "modal-content",
     },
-    mask: {
-        class: "p-dialog-mask p-component-overlay p-component-overlay-enter",
-    },
     header: {
         class: "modal-header",
     },
@@ -70,52 +67,3 @@ const getSizeClass = computed(() => {
         </template>
     </prime-dialog>
 </template>
-
-<style lang="scss">
-:root {
-    --maskbg: rgba(0, 0, 0, 0.07);
-}
-
-.modal-content {
-    max-height: 90%;
-}
-
-.modal-body {
-    overflow-y: auto;
-}
-
-.p-component-overlay {
-    background-color: var(--maskbg);
-    transition-duration: 0.2s;
-}
-
-.p-dialog-mask.p-component-overlay {
-    pointer-events: auto;
-}
-
-.p-component-overlay-enter {
-    animation: p-component-overlay-enter-animation 150ms forwards;
-}
-
-.p-component-overlay-leave {
-    animation: p-component-overlay-leave-animation 150ms forwards;
-}
-
-@keyframes p-component-overlay-enter-animation {
-    from {
-        background-color: transparent;
-    }
-    to {
-        background-color: var(--maskbg);
-    }
-}
-
-@keyframes p-component-overlay-leave-animation {
-    from {
-        background-color: var(--maskbg);
-    }
-    to {
-        background-color: transparent;
-    }
-}
-</style>
