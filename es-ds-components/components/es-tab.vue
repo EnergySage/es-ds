910dd6b9811970918ee8783a90140cc402ebb363
ericdouglaspratt <eric.pratt@energysage.com>
2024-08-29 14:02:27 -0400

    feat: added EsTabs wrapper component for PrimeVue TabView

---
+++ es-ds-components/components/es-tab.vue
@@ -0,0 +1,6 @@
+<template>
+    <!--
+    While this file needs to exist to keep Nuxt happy, it doesn't actually do anything.
+    Instances of es-tab are placed inside es-tabs, and es-tabs handles their display.
+    -->
+</template>
---
+++ es-ds-components/components/es-tabs.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import TabPanel from 'primevue/tabpanel';
+import TabView from 'primevue/tabview';
+
+// allow use of v-model on this component
+const model = defineModel();
+
+// get the list of elements provided as children to the default slot
+const panels = useSlots().default?.() || [];
+
+// keep track of the active tab index
+//  - from above, if v-model is used
+//  - from below, if the tab component receives a click
+const activeIndex = ref(0);
+
+// watch the v-model for changes and update the active index if it does change
+watch(model, (newVal) => {
+    activeIndex.value = newVal;
+});
+
+// called from the TabView when the user clicks a tab to make it active
+const updateActiveIndex = (index) => {
+    model.value = index;
+};
+</script>
+
+<template>
+    <tab-view
+        :active-index="activeIndex"
+        @update:active-index="updateActiveIndex"
+        :pt="{
+            root: 'tabs es-tabs',
+            nav: 'list-unstyled nav position-relative',
+            tab: 'nav-link',
+            inkbar: 'inkbar position-absolute',
+            panelContainer: 'tab-content'
+        }">
+        <tab-panel
+            v-for="(panel, index) in panels"
+            :key="index"
+            :header="panel.props.title"
+            :pt="{
+                header: 'nav-item',
+                headerAction: ({ context }) => ({
+                    class: [
+                        'nav-link',
+                        {
+                            'active active-tab font-weight-bolder': context.active,
+                        },
+                    ],
+                }),
+                content: ({ context }) => ({
+                    class: [
+                        'tab-pane mt-100',
+                        {
+                            'active': context.active
+                        }
+                    ]
+                })
+            }">
+            <component v-for="item in panel.children.default()" :is="item" />
+        </tab-panel>
+    </tab-view>
+</template>
