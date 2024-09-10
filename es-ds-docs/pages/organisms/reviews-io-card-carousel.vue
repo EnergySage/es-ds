b4ba5aee8ea0fb07bbc017c0edaf5adc7189c900
Hannah Roth <hannah.roth@energysage.com>
2024-09-10 16:06:31 -0400

    feat: add EsReviewsIoCardCarousel v3

---
+++ es-ds-docs/pages/organisms/reviews-io-card-carousel.vue
@@ -0,0 +1,38 @@
+
+<script setup>
+const { $prism } = useNuxtApp();
+const compCode = ref('');
+const docCode = ref('');
+
+if ($prism) {
+    /* eslint-disable import/no-webpack-loader-syntax, import/no-self-import */
+    const compSource = await import('@energysage/es-ds-components/components/es-reviews-io-card-carousel.vue?raw');
+    const docSource = await import('./reviews-io-card-carousel.vue?raw');
+    /* eslint-enable import/no-webpack-loader-syntax, import/no-self-import */
+
+    compCode.value = $prism.normalizeCode(compSource.default);
+    docCode.value = $prism.normalizeCode(docSource.default);
+    $prism.highlight();
+}
+</script>
+
+<template>
+    <div>
+        <h1>
+            Reviews.io card carousel
+        </h1>
+        <p>
+            This is a third-party widget provided by Reviews.io with custom styling on our end.
+        </p>
+
+        <div class="my-500">
+            <es-reviews-io-card-carousel />
+        </div>
+    </div>
+
+    <ds-doc-source
+        :comp-code="compCode"
+        comp-source="es-ds-components/src/lib-components/es-reviews-io-card-carousel.vue"
+        :doc-code="docCode"
+        doc-source="es-ds-docs/pages/molecules/reviews-io-carousel.vue" />
+</template>
