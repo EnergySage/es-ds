ed130190507141ff80d0d3f72056b7eff526f004
Lia Eggleston <lia.eggleston@energysage.com>
2024-09-10 11:26:02 -0400

    feat: v3 es-view-more

---
+++ es-ds-components/components/es-view-more.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import truncate from 'html-truncate';
+
+const props = defineProps({
+    /**
+     * Content of the button
+     */
+    content: {
+        type: String,
+        required: true,
+    },
+    /**
+     * Maximum character length of text before it is truncated
+     */
+    length: {
+        type: Number,
+        required: true,
+    },
+    /**
+     * Text for button to show more text
+     */
+    clamp: {
+        type: String,
+        required: false,
+        default: 'Show more',
+    },
+    /**
+     * Text for button to hide text
+     */
+    less: {
+        type: String,
+        required: false,
+        default: 'Show less',
+    },
+});
+
+const isClamped = ref(true);
+
+const truncatedContent = computed(() => truncate(props.content, props.length));
+const isTruncated = computed(() => truncatedContent.value !== props.content);
+const buttonText = computed(() => (isClamped.value ? props.clamp : props.less));
+const bodyContent = computed(() => {
+    if (isClamped.value) {
+        return truncatedContent.value;
+    }
+    return props.content;
+});
+</script>
+
+<template>
+    <div>
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <span v-html="bodyContent" />
+        <es-button
+            v-if="isTruncated"
+            inline
+            variant="link"
+            class="ml-50 p-0"
+            @click="isClamped = !isClamped">
+            {{ buttonText }}
+        </es-button>
+    </div>
+</template>
---
+++ es-ds-docs/pages/molecules/view-more.vue
@@ -0,0 +1,37 @@
+<script setup>
+const { $prism } = useNuxtApp();
+const compCode = ref('');
+const docCode = ref('');
+if ($prism) {
+    const compSource = await import('@energysage/es-ds-components/components/es-view-more.vue?raw');
+    // eslint-disable-next-line import/no-self-import
+    const docSource = await import('./view-more.vue?raw');
+    compCode.value = $prism.normalizeCode(compSource.default);
+    docCode.value = $prism.normalizeCode(docSource.default);
+    $prism.highlight();
+}
+</script>
+
+<template>
+    <div>
+        <h1>View more</h1>
+
+        <es-view-more
+            class="my-500"
+            :length="100"
+            :content="`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in aliquam ex.
+                Nullam vestibulum ex mi, ut suscipit libero condimentum id. Pellentesque eu diam vel nisi
+                molestie porta eget sed odio. Quisque congue risus id metus facilisis, non imperdiet libero rutrum.
+                Mauris vitae ante porttitor, consectetur purus faucibus, euismod ex. Orci varius natoque penatibus
+                et magnis dis parturient montes, nascetur ridiculus mus. Nulla ullamcorper elit sed viverra finibus.
+                Mauris vitae tortor mauris. Cras suscipit nibh nec nisi cursus ornare. Maecenas quis turpis sit amet
+                sapien dapibus sollicitudin viverra eu justo. Vivamus posuere metus sit amet purus tempus volutpat.
+                Donec eleifend elit quam. `" />
+
+        <ds-doc-source
+            :comp-code="compCode"
+            comp-source="es-ds-components/components/es-view-more.vue"
+            :doc-code="docCode"
+            doc-source="es-ds-docs/pages/molecules/view-more.vue" />
+    </div>
+</template>
