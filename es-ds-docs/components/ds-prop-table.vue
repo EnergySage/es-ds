ce106bc8336073a6d8f9a0b0496dde0fba20de0f
Hannah Roth <hannah.roth@energysage.com>
2024-09-12 13:28:58 -0400

    chore: add comment

--- es-ds-docs/components/ds-prop-table.vue
+++ es-ds-docs/components/ds-prop-table.vue
@@ -36,6 +36,7 @@ export default {
                 <template #value>
                     <code
                         v-if="
+                            // Applies code formatting to the first 2 out of 3 columns or 3 out of 4+ columns
                             ((widths.md.length > 3 && columnIndex < 3) || columnIndex < 2) &&
                             row[columnIndex] !== 'n/a'
                         ">
