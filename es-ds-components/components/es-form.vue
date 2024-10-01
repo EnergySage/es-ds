a8c1ba61c0ef3c620710e9dce78e7d3ec5d2e923
Hannah Roth <hannah.roth@energysage.com>
2024-10-01 15:20:24 -0400

    feat: add EsForm V3

---
+++ es-ds-components/components/es-form.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const props = defineProps({
+    /**
+     * Inline
+     */
+    inline: {
+        type: Boolean,
+        default: false,
+    },
+    novalidate: {
+        type: Boolean,
+        default: false,
+    },
+    validated: {
+        type: Boolean,
+        default: false,
+    }
+});
+</script>
+
+<template>
+    <form
+        :class="{ 'form-inline': props.inline }"
+        :novalidate="props.novalidate">
+        <slot />
+    </form>
+</template>
+
+<style lang="scss">
+.form-inline {
+    align-items: center;
+    display: flex;
+    flex-flow: row wrap;
+
+    &.input-wrapper {
+        justify-content: unset !important;
+        display: flex;
+        flex: 0 0 100%;
+    }
+
+    &.input-wrapper label {
+        flex: 0 0 30%;
+    }
+
+    &.input-wrapper .input-holder {
+        flex: 0 0 70%;
+    }
+}
+</style>
---
+++ es-ds-docs/pages/organisms/form.vue
@@ -0,0 +1,154 @@
+
+<script setup lang="ts">
+
+const inline = ref(false);
+
+const propTableRows = [
+    ['inline', 'Boolean', 'false', 'Display form inline'],
+    ['novalidate', 'Boolean', 'false', 'Disable browser validation'],
+    ['validated', 'Boolean', 'false', 'Determines if the form is validated'],
+];
+
+
+const state = reactive({
+    form: {
+        email: '',
+        password: '',
+        phone: '',
+        maskedPhoneNumber: '',
+        notes: '',
+    },
+});
+
+const rules = {};
+
+const { v$, isSubmitInProgress, startSubmit, stopSubmit } = useEsForms(
+    rules,
+    state,
+);
+
+const { $prism } = useNuxtApp();
+const docCode = ref('');
+onMounted(async () => {
+    if ($prism) {
+        // eslint-disable-next-line import/no-self-import
+        const docSource = await import('./form.vue?raw');
+
+        docCode.value = $prism.normalizeCode(docSource.default);
+        $prism.highlight();
+    }
+});
+
+const asyncTimeout = async (seconds = 3) => {
+    const millisecondTimeout = seconds * 1000;
+    return new Promise((resolve) => {
+        setTimeout(resolve, millisecondTimeout);
+    });
+};
+
+const fakeServerRequest = async () => {
+    await asyncTimeout();
+    // eslint-disable-next-line no-console
+    console.log('Submit Complete!');
+};
+
+const onSubmit = async () => {
+    startSubmit();
+    await fakeServerRequest();
+    stopSubmit();
+};
+</script>
+
+<template>
+    <b-container>
+        <b-row>
+            <b-col>
+                <h1>
+                    Form
+                </h1>
+                <es-form-checkbox
+                    id="inline-form"
+                    v-model="inline"
+                    name="inline-form">
+                    Inline form
+                </es-form-checkbox>
+            </b-col>
+        </b-row>
+        <b-row class="my-500 border-top pt-500">
+            <b-col
+                cols="12"
+                lg="8">
+                <es-form
+                    :inline="inline"
+                    @submit.stop.prevent="onSubmit">
+                    <es-form-input
+                        id="email"
+                        v-model="state.form.email"
+                        :disabled="isSubmitInProgress"
+                        required>
+                        <template #label>
+                            Email address
+                        </template>
+                        <template #errorMessage>
+                            Please enter a valid email address.
+                        </template>
+                    </es-form-input>
+                    <es-form-input
+                        id="password"
+                        v-model="state.form.password"
+                        :disabled="isSubmitInProgress"
+                        required
+                        type="tel">
+                        <template #label>
+                            Password
+                        </template>
+                    </es-form-input>
+                    <es-form-input
+                        id="phone"
+                        v-model="state.form.phone"
+                        :disabled="isSubmitInProgress"
+                        required
+                        type="tel">
+                        <template #label>
+                            Phone number
+                        </template>
+                        <template #errorMessage>
+                            Please enter a valid phone number.
+                        </template>
+                    </es-form-input>
+                    <es-form-textarea
+                        id="notes"
+                        v-model="state.form.notes"
+                        :disabled="isSubmitInProgress"
+                        required>
+                        <template #label>
+                            Notes
+                        </template>
+                    </es-form-textarea>
+                    <div class="d-flex flex-grow-1 justify-content-end mt-200">
+                        <es-button
+                            type="submit"
+                            class="w-100 w-lg-auto"
+                            :disabled="isSubmitInProgress">
+                            <span class="w-100">
+                                <span v-if="isSubmitInProgress">
+                                    <b-spinner
+                                        role="status"
+                                        label="Loading" />
+                                </span>
+                                <span :class="{ 'sr-only': isSubmitInProgress }"> Submit </span>
+                            </span>
+                        </es-button>
+                    </div>
+                </es-form>
+            </b-col>
+        </b-row>
+        <div class="my-500">
+            <h2>EsForm props</h2>
+            <ds-prop-table :rows="propTableRows" />
+        </div>
+        <ds-doc-source
+            :doc-code="docCode"
+            doc-source="es-design-system/pages/organisms/es-form.vue" />
+    </b-container>
+</template>
