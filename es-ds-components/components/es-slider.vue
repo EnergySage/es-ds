ec80d103cb2d80be0e22857776dbdde5dca98528
Tommy <tomjleo@gmail.com>
2024-10-07 10:23:24 -0400

    feat: use v-bind in the style section

--- es-ds-components/components/es-slider.vue
+++ es-ds-components/components/es-slider.vue
@@ -47,15 +47,17 @@ const props = defineProps({
     labelFormatter: {
         type: Function,
         required: false,
-        default: (val) => val,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        default: (val: any) => val,
     },
 });
 
 // allow use of v-model on this component
-const model = defineModel<number>();
-
-// set starting value
+const model = defineModel<number | number[]>();
 model.value = props.startingValue;
+function handleSliderUpdate(newVal: number | number[]) {
+    model.value = newVal;
+}
 </script>
 
 <template>
@@ -76,8 +78,9 @@ model.value = props.startingValue;
                 },
                 handle: {
                     class: 'slider-handle',
-                },
-            }" />
+                }
+            }"
+            @update:model-value="handleSliderUpdate" />
 
         <div class="d-flex flex-row justify-content-between">
             <span>{{ labelFormatter(min) }}</span>
@@ -126,7 +129,7 @@ model.value = props.startingValue;
     bottom: 27px;
     box-shadow: 0 1px 6px 0 rgba(34, 38, 51, 0.25);
     color: variables.$white;
-    content: attr(aria-valuenow);
+    content: v-bind("`'${model}'`");
     display: flex;
     font-weight: variables.$font-weight-boldest;
     height: 52px;
