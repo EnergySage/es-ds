5b8037365430c9649781f16b1042eac230072025
ericdouglaspratt <eric.pratt@energysage.com>
2024-10-31 16:31:59 -0400

    feat: EsCarousel custom responsiveness and more props exposed

--- es-ds-components/components/es-carousel.vue
+++ es-ds-components/components/es-carousel.vue
@@ -1,50 +1,125 @@
 <script setup lang="ts">
+/*
+    TODO:
+     - why is mobile so messed up with paging
+     - why does mobile first show item 12 and then switch to item 1
+*/
+
 import Carousel from 'primevue/carousel';
+import sassBreakpoints from '@energysage/es-ds-styles/scss/modules/breakpoints.module.scss';
+import type { EsCarouselBreakpointsInterface } from '../types';
 
-const responsiveOptions = ref([
-    {
-        // xl: 1200px
-        breakpoint: '1199px',
-        numVisible: 4,
-        numScroll: 1,
-    },
-    {
-        // lg: 992px
-        breakpoint: '991px',
-        numVisible: 3,
-        numScroll: 1,
-    },
-    {
-        // md: 768px
-        breakpoint: '767px',
-        numVisible: 2,
-        numScroll: 1,
-    },
-    {
-        // sm: 576px
-        breakpoint: '575px',
-        numVisible: 1,
-        numScroll: 1,
-    },
-]);
-
-const props = defineProps({
-    autoscroll: {
-        type: Boolean,
-        default: false,
-    },
-    dots: {
-        type: Boolean,
-        default: true,
-    },
-    items: {
-        type: Array,
-        default: () => [],
-        required: true,
-    },
+interface IProps {
+    autoPlay?: boolean;
+    autoPlayInterval?: number;
+    breakpoints?: EsCarouselBreakpointsInterface;
+    circular?: boolean;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    items: Array<any>;
+    numScroll?: number;
+    numVisible?: number;
+    showArrows?: boolean;
+    showDots?: boolean;
+}
+const props = withDefaults(defineProps<IProps>(), {
+    autoPlay: false,
+    autoPlayInterval: 2000,
+    breakpoints: () => ({}),
+    circular: true,
+    items: () => [],
+    numScroll: 1,
+    numVisible: 1,
+    showArrows: true,
+    showDots: true,
 });
 
-const autoplayInterval = ref(props.autoscroll ? 3000 : 0);
+const emit = defineEmits<{
+    update: [value: number];
+}>();
+
+// get the breakpoint pixel numbers from the SASS variables in es-ds-styles
+const parseSassBreakpoint = (breakpoint: string) => parseInt(breakpoint.replace('px', ''), 10);
+const BREAKPOINTS = {
+    SM: parseSassBreakpoint(sassBreakpoints.sm),
+    MD: parseSassBreakpoint(sassBreakpoints.md),
+    LG: parseSassBreakpoint(sassBreakpoints.lg),
+    XL: parseSassBreakpoint(sassBreakpoints.xl),
+    XXL: parseSassBreakpoint(sassBreakpoints.xxl),
+};
+
+// lower breakpoint values propagate to higher breakpoints unless overridden
+const numVisibleXs = computed(() => props.numVisible);
+const numVisibleSm = computed(() => props.breakpoints?.sm?.numVisible || numVisibleXs.value);
+const numVisibleMd = computed(() => props.breakpoints?.md?.numVisible || numVisibleSm.value);
+const numVisibleLg = computed(() => props.breakpoints?.lg?.numVisible || numVisibleMd.value);
+const numVisibleXl = computed(() => props.breakpoints?.xl?.numVisible || numVisibleLg.value);
+const numVisibleXxl = computed(() => props.breakpoints?.xxl?.numVisible || numVisibleXl.value);
+
+// lower breakpoint values propagate to higher breakpoints unless overridden
+const numScrollXs = computed(() => props.numScroll);
+const numScrollSm = computed(() => props.breakpoints?.sm?.numScroll || numScrollXs.value);
+const numScrollMd = computed(() => props.breakpoints?.md?.numScroll || numScrollSm.value);
+const numScrollLg = computed(() => props.breakpoints?.lg?.numScroll || numScrollMd.value);
+const numScrollXl = computed(() => props.breakpoints?.xl?.numScroll || numScrollLg.value);
+const numScrollXxl = computed(() => props.breakpoints?.xxl?.numScroll || numScrollXl.value);
+
+// in order to enable full-width carousels with one item
+// but also ensure there is some space between items when more than one is visible
+// only apply horizontal padding to carousel items on breakpoints where more than one item is visible
+const DEFAULT_PADDING = '0.5rem';
+const paddingXs = computed(() => (numVisibleXs.value === 1 ? '0' : DEFAULT_PADDING));
+const paddingSm = computed(() => (numVisibleSm.value === 1 ? '0' : DEFAULT_PADDING));
+const paddingMd = computed(() => (numVisibleMd.value === 1 ? '0' : DEFAULT_PADDING));
+const paddingLg = computed(() => (numVisibleLg.value === 1 ? '0' : DEFAULT_PADDING));
+const paddingXl = computed(() => (numVisibleXl.value === 1 ? '0' : DEFAULT_PADDING));
+const paddingXxl = computed(() => (numVisibleXxl.value === 1 ? '0' : DEFAULT_PADDING));
+
+const responsiveOptions = computed(() => {
+    // if no special breakpoints are defined, don't pass in any responsive options
+    if (!Object.keys(props.breakpoints).length) {
+        return undefined;
+    }
+
+    return [
+        // XXL breakpoint
+        {
+            // max width of XXL is infinite, so let's use 9999px
+            breakpoint: '9999px',
+            numScroll: numScrollXxl.value,
+            numVisible: numVisibleXxl.value,
+        },
+        // XL breakpoint
+        {
+            // max width of XL is XXL minus one
+            breakpoint: `${BREAKPOINTS.XXL - 1}px`,
+            numScroll: numScrollXl.value,
+            numVisible: numVisibleXl.value,
+        },
+        // LG breakpoint
+        {
+            // max width of LG is XL minus one
+            breakpoint: `${BREAKPOINTS.XL - 1}px`,
+            numScroll: numScrollLg.value,
+            numVisible: numVisibleLg.value,
+        },
+        // MD breakpoint
+        {
+            // max width of MD is LG minus one
+            breakpoint: `${BREAKPOINTS.LG - 1}px`,
+            numScroll: numScrollMd.value,
+            numVisible: numVisibleMd.value,
+        },
+        // SM breakpoint
+        {
+            // max width of SM is MD minus one
+            breakpoint: `${BREAKPOINTS.MD - 1}px`,
+            numScroll: numScrollSm.value,
+            numVisible: numVisibleSm.value,
+        },
+    ];
+});
+
+const autoplayInterval = ref(props.autoPlay ? props.autoPlayInterval : 0);
 const key = ref('');
 
 const stopAutoplay = () => {
@@ -69,20 +144,22 @@ onMounted(() => {
     <carousel
         :key="key"
         :autoplay-interval="autoplayInterval"
-        circular
-        :num-visible="4"
+        :circular="props.circular"
+        :num-scroll="props.numScroll"
+        :num-visible="props.numVisible"
         :responsive-options="responsiveOptions"
-        :show-indicators="dots"
-        :value="items"
+        :show-indicators="props.showDots"
+        :show-navigators="props.showArrows"
+        :value="props.items"
         :pt="{
             container: {
                 class: 'd-flex',
             },
             indicator: {
-                class: 'dot',
+                class: 'es-carousel-dot',
             },
             indicators: {
-                class: 'd-flex justify-content-center',
+                class: 'es-carousel-dots d-flex justify-content-center mt-100',
                 style: 'gap: 12px;',
             },
             itemsContent: {
@@ -92,22 +169,29 @@ onMounted(() => {
                 class: 'd-flex',
             },
             item: {
-                class: 'p-carousel-item p-1',
+                class: 'es-carousel-item',
             },
             itemCloned: {
-                class: 'p-carousel-item p-1',
+                class: 'es-carousel-item',
             },
             previousButton: {
-                style: 'border: unset; background: unset; color: #64748b;',
+                class: 'es-carousel-arrow es-carousel-prev-arrow btn btn-outline-primary px-sm-50',
             },
             nextButton: {
-                style: 'border: unset; background: unset; color: #64748b;',
+                class: 'es-carousel-arrow es-carousel-next-arrow btn btn-outline-primary px-sm-50',
             },
-        }">
+        }"
+        @update:page="(value: number) => emit('update', value)">
         <template #item="item">
             <slot
                 name="item"
                 :item="item.data" />
+        </template>
+        <template #previousicon>
+            <icon-chevron-left />
+        </template>
+        <template #nexticon>
+            <icon-chevron-right />
         </template>
     </carousel>
 </template>
@@ -116,29 +200,64 @@ onMounted(() => {
 @use '@energysage/es-ds-styles/scss/variables' as variables;
 @use '@energysage/es-ds-styles/scss/mixins/breakpoints' as breakpoints;
 
-:deep(.p-carousel-item) {
-    flex: 1 0 100%;
+:deep(.es-carousel-item) {
+    flex: 1 0 calc(100% / v-bind(numVisibleXs));
+    padding: 0 v-bind(paddingXs);
 }
 
 @include breakpoints.media-breakpoint-up(sm) {
-    :deep(.p-carousel-item) {
-        flex: 1 0 calc(100% / 2);
+    :deep(.es-carousel-item) {
+        flex: 1 0 calc(100% / v-bind(numVisibleSm));
+        padding: 0 v-bind(paddingSm);
     }
 }
 
 @include breakpoints.media-breakpoint-up(md) {
-    :deep(.p-carousel-item) {
-        flex: 1 0 calc(100% / 3);
+    :deep(.es-carousel-item) {
+        flex: 1 0 calc(100% / v-bind(numVisibleMd));
+        padding: 0 v-bind(paddingMd);
     }
 }
 
 @include breakpoints.media-breakpoint-up(lg) {
-    :deep(.p-carousel-item) {
-        flex: 1 0 calc(100% / 4);
+    :deep(.es-carousel-item) {
+        flex: 1 0 calc(100% / v-bind(numVisibleLg));
+        padding: 0 v-bind(paddingLg);
     }
 }
 
-:deep(.dot) {
+@include breakpoints.media-breakpoint-up(xl) {
+    :deep(.es-carousel-item) {
+        flex: 1 0 calc(100% / v-bind(numVisibleXl));
+        padding: 0 v-bind(paddingXl);
+    }
+}
+
+@include breakpoints.media-breakpoint-up(xxl) {
+    :deep(.es-carousel-item) {
+        flex: 1 0 calc(100% / v-bind(numVisibleXxl));
+        padding: 0 v-bind(paddingXxl);
+    }
+}
+
+:deep(.es-carousel-arrow) {
+    background: unset;
+    border: unset;
+    box-shadow: none;
+    color: variables.$gray-900;
+    height: auto;
+
+    &:hover {
+        color: variables.$gray-700;
+    }
+    &:not(:disabled):not(.disabled):active {
+        background: unset;
+        box-shadow: none;
+        color: variables.$gray-700;
+    }
+}
+
+:deep(.es-carousel-dot) {
     list-style-type: none;
 
     &[data-p-highlight='true'] button {
