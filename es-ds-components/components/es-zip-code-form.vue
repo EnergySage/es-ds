80f83f35bb3f42f2397f3b31395132adff25d7c2
Etelson Alcius <etelson.alcius@energysage.com>
2025-02-19 14:02:01 -0500

    refactor: Added separate v-if=contextMessage block and kept the rest of the form layout.

--- es-ds-components/components/es-zip-code-form.vue
+++ es-ds-components/components/es-zip-code-form.vue
@@ -102,98 +102,98 @@ const handleSubmit = () => {
 
 <template>
     <div
-        class="EsZipCodeForm d-flex"
-        :class="{
-            'EsZipCodeForm--constrained': constrained,
-            'text-white': dark,
-        }"
-        v-bind="$attrs">
-        <div class="d-flex flex-column">
-            <form
-                ref="ctaForm"
-                class="justify-content-center w-100"
-                :class="{
-                    [`d-${stackBreak}flex`]: stackUntil,
-                    'mb-100': showPrivacySection,
-                }"
-                :action="url"
-                method="get"
-                novalidate
-                :target="newTab ? '_blank' : '_self'"
-                @submit.prevent.stop="handleSubmit">
-                <es-form-input
-                    :id="inputId"
-                    v-model="state.zipCode"
-                    :state="validateState('zipCode')"
-                    autocomplete="postal-code"
-                    :class="{
-                        [`mb-${stackBreak}0 mr-${stackBreak}50`]: stackUntil,
-                    }"
-                    inputmode="numeric"
-                    label-sr-only
-                    maxlength="5"
-                    :name="fieldName"
-                    pattern="\d*"
-                    :placeholder="placeholder"
-                    required>
-                    <template
-                        v-if="contextMessage"
-                        #extraContext>
-                        {{ contextMessage }}
-                    </template>
-                    <template #prefixIcon>
-                        <icon-location class="text-gray-800" />
-                    </template>
-                    <template #label>
-                        {{ placeholder }}
-                    </template>
-                    <template #errorMessage>
-                        <span :class="{ 'text-white': dark }">
-                            <slot name="errorMessage"> Please enter a 5-digit zip code. </slot>
-                        </span>
-                    </template>
-                </es-form-input>
-                <input
-                    v-if="selectedProduct"
-                    type="hidden"
-                    name="product"
-                    :value="selectedProduct" />
-                <es-button
-                    class="text-nowrap w-100"
-                    :class="{
-                        [`ml-${stackBreak}50 w-${stackBreak}auto`]: stackUntil,
-                        'px-100': constrained,
-                    }"
-                    type="submit"
-                    :variant="dark ? 'dark-bg' : 'primary'">
-                    <slot name="buttonText"> Submit </slot>
-                </es-button>
-            </form>
-            <div
-                v-if="showPrivacySection"
-                class="d-flex text-left"
-                :class="{ 'font-size-75': constrained }">
-                <icon-lock-on
-                    aria-hidden="true"
-                    class="privacy-lock-icon flex-shrink-0 mr-50 position-relative"
-                    :class="{ 'mt-25': !constrained }"
-                    height="1.125rem"
-                    width="1.125rem" />
-                <div>
-                    <span>
-                        <slot name="privacyExplanation"> Your information is safe with us. </slot>
-                    </span>
-                    <nuxt-link
-                        v-if="privacyPolicyLink"
-                        :href="privacyPolicyLink"
-                        class="text-nowrap"
-                        :class="dark ? 'text-white' : ''"
-                        :target="privacyPolicyNewTab ? '_blank' : '_self'">
-                        <slot name="privacyPolicyLinkText"> Privacy Policy </slot>
-                    </nuxt-link>
-                </div>
-            </div>
+      class="EsZipCodeForm d-flex"
+      :class="{
+        'EsZipCodeForm--constrained': constrained,
+        'text-white': dark,
+      }"
+      v-bind="$attrs">
+      <div class="d-flex flex-column">
+        <div v-if="contextMessage" class="mb-2">
+          {{ contextMessage }}
         </div>
+          <form
+          ref="ctaForm"
+          class="justify-content-center w-100"
+          :class="{
+            [`d-${stackBreak}flex`]: stackUntil,
+            'mb-100': showPrivacySection,
+          }"
+          :action="url"
+          method="get"
+          novalidate
+          :target="newTab ? '_blank' : '_self'"
+          @submit.prevent.stop="handleSubmit">
+          <es-form-input
+            :id="inputId"
+            v-model="state.zipCode"
+            :state="validateState('zipCode')"
+            autocomplete="postal-code"
+            :class="{
+              [`mb-${stackBreak}0 mr-${stackBreak}50`]: stackUntil,
+            }"
+            inputmode="numeric"
+            label-sr-only
+            maxlength="5"
+            :name="fieldName"
+            pattern="\d*"
+            :placeholder="placeholder"
+            required>
+            <template #prefixIcon>
+              <icon-location class="text-gray-800"/>
+            </template>
+            <template #label>
+              {{ placeholder }}
+            </template>
+            <template #errorMessage>
+              <span :class="{ 'text-white': dark }">
+                <slot name="errorMessage">Please enter a 5-digit zip code.</slot>
+              </span>
+            </template>
+          </es-form-input>
+          <input
+            v-if="selectedProduct"
+            type="hidden"
+            name="product"
+            :value="selectedProduct"/>
+          <es-button
+            class="text-nowrap w-100"
+            :class="{
+              [`ml-${stackBreak}50 w-${stackBreak}auto`]: stackUntil,
+              'px-100': constrained,
+            }"
+            type="submit"
+            :variant="dark ? 'dark-bg' : 'primary'">
+            <slot name="buttonText">Submit</slot>
+          </es-button>
+        </form>
+        <div
+          v-if="showPrivacySection"
+          class="d-flex text-left"
+          :class="{ 'font-size-75': constrained }">
+          <icon-lock-on
+            aria-hidden="true"
+            class="privacy-lock-icon flex-shrink-0 mr-50 position-relative"
+            :class="{ 'mt-25': !constrained }"
+            height="1.125rem"
+            width="1.125rem"/>
+          <div>
+            <span>
+              <slot name="privacyExplanation">
+                Your information is safe with us.
+              </slot>
+            </span>
+            <nuxt-link
+              v-if="privacyPolicyLink"
+              :href="privacyPolicyLink"
+              class="text-nowrap"
+              :class="dark ? 'text-white' : ''"
+              :target="privacyPolicyNewTab ? '_blank' : '_self'">
+              <slot name="privacyPolicyLinkText">Privacy Policy</slot>
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
@@ -206,12 +206,6 @@ const handleSubmit = () => {
     &--constrained {
         .privacy-lock-icon {
             top: 0.02em;
-        }
-    }
-    form {
-        align-items: flex-start !important;
-        &:not(:has([data-context-message])) {
-            align-items: flex-end !important;
         }
     }
 }
