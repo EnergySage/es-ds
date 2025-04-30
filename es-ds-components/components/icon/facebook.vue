e3ef5c6d43b0f87e888d685184b49b694d2d987b
ericdouglaspratt <eric.pratt@energysage.com>
2025-04-30 11:14:58 -0400

    fix: updated all old social icons to match new 32x32 size

--- es-ds-components/components/icon/facebook.vue
+++ es-ds-components/components/icon/facebook.vue
@@ -11,19 +11,17 @@ withDefaults(defineProps<IProps>(), {
 
 <template>
     <svg
+        fill="none"
         :style="{
             height: height,
             width: width,
         }"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 32 32"
-        fill="currentColor">
-        <path
-            fill="none"
-            d="M0 .4h31v31H0z" />
+        xmlns="http://www.w3.org/2000/svg">
         <path
             fill-rule="evenodd"
             clip-rule="evenodd"
-            d="M15.5.4a15.5 15.5 0 1 0 0 31 15.5 15.5 0 0 0 0-31Zm5.7 8.1h-2a2 2 0 0 0-1.6.5c-.3.4-.4 1-.4 1.4V13h3.9l-.5 4h-3.4v10h-4V17H9.8v-4h3.4v-2.8a5.1 5.1 0 0 1 1.3-3.9A5 5 0 0 1 18.2 5l3 .1v3.5Z" />
+            d="M16 .163a15.837 15.837 0 1 0 0 31.674A15.837 15.837 0 0 0 16 .163Zm5.824 8.277H19.78a2.044 2.044 0 0 0-1.634.51c-.306.409-.41 1.022-.41 1.43v2.657h3.985l-.51 4.088h-3.474V27.34H13.65V17.124h-3.474v-4.087h3.474v-2.86a5.21 5.21 0 0 1 1.328-3.986 5.109 5.109 0 0 1 3.78-1.328l3.066.102v3.576z"
+            fill="currentColor"
+            style="stroke-width: 1.15477" />
     </svg>
 </template>
