5f1d2397fa91d2542a26896bfb33b0c62039d837
ericdouglaspratt <eric.pratt@energysage.com>
2025-07-07 18:03:40 -0400

    feat: match EsProgress to design

--- es-ds-components/components/es-progress.vue
+++ es-ds-components/components/es-progress.vue
@@ -1,36 +1,95 @@
 <script setup lang="ts">
 import ProgressBar from 'primevue/progressbar';
 
-const props = defineProps({
-    value: {
-        type: Number,
-        required: true,
-    },
-    height: {
-        type: String,
-        default: '0.3125rem',
-    },
-    showValue: {
-        type: Boolean,
-        default: false,
-    },
+const BASE_FONT_SIZE = 16;
+const DEFAULT_BAR_HEIGHT = 2;
+const DEFAULT_BORDER_WIDTH = 0.5;
+const DEFAULT_CIRCLE_GLOW_SIZE = 4;
+const DEFAULT_INSET_SHADOW_SIZE = 0.5;
+const DIFF_CIRCLE_SIZE_FROM_BAR_HEIGHT_IN_PX = 4;
+const DEFAULT_CIRCLE_DIMENSION = DEFAULT_BAR_HEIGHT + DIFF_CIRCLE_SIZE_FROM_BAR_HEIGHT_IN_PX;
+
+interface Props {
+    formatter?: Function;
+    height?: string;
+    showCircle?: boolean;
+    showValue?: boolean;
+    value: number;
+    valueClass?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    formatter: (val: number) => `${val}%`,
+    height: '0.125rem',
+    showCircle: true,
+    showValue: false,
+    valueClass: '',
 });
+
 const progressBarPt = {
     value: {
-        class: 'EsProgressValue progress-bar',
+        class: [
+            'EsProgressValue progress-bar overflow-visible position-relative rounded-sm',
+            {
+                'progress-bar--with-circle': props.showCircle,
+            },
+        ],
         style: {
             height: props.height,
-            borderRadius: props.value === 100 ? '0' : '0 0.125rem 0.125rem 0',
         },
     },
 };
+
+const barHeight = computed(() => {
+    let barHeight = DEFAULT_BAR_HEIGHT;
+
+    if (props.height.indexOf('rem') === props.height.length - 3) {
+        barHeight = parseFloat(props.height.replace('rem', '')) * BASE_FONT_SIZE;
+    } else if (props.height.indexOf('px') === props.height.length - 2) {
+        barHeight = parseInt(props.height.replace('px', ''), 10);
+    }
+
+    return barHeight;
+});
+const circleDimension = computed(() => barHeight.value + (DIFF_CIRCLE_SIZE_FROM_BAR_HEIGHT_IN_PX * barHeight.value) / DEFAULT_BAR_HEIGHT);
+
+const circleDimensionPx = computed(() => `${circleDimension.value}px`);
+const circleRightPx = computed(() => `${circleDimension.value * -0.5}px`);
+const circleBorderWidthPx = computed(() => `${(DEFAULT_BORDER_WIDTH * circleDimension.value / DEFAULT_CIRCLE_DIMENSION)}px`);
+const circleGlowSizePx = computed(() => `${DEFAULT_CIRCLE_GLOW_SIZE * circleDimension.value / DEFAULT_CIRCLE_DIMENSION}px`)
+const circleInsetShadowSizePx = computed(() => `${DEFAULT_INSET_SHADOW_SIZE * circleDimension.value / DEFAULT_CIRCLE_DIMENSION}px`);
+
+const verticalContainerPaddingPx = computed(() => props.showCircle ? `${(circleDimension.value - barHeight.value) / 2}px` : '0');
 </script>
+
 <template>
-    <progress-bar
-        class="bg-gray-200 rounded-0"
-        :pt="progressBarPt"
-        :show-value="showValue"
-        :value="value"
-        v-bind="$attrs">
-    </progress-bar>
+    <div class="es-progress" :class="$attrs.class">
+        <progress-bar
+            class="bg-gray-200 rounded-sm"
+            :pt="progressBarPt"
+            :show-value="false"
+            :value="value"
+            v-bind="omit('class', $attrs)">
+        </progress-bar>
+        <div v-if="showValue" class="font-size-75 mt-25 text-right" :class="valueClass">
+            {{ formatter(value) }}
+        </div>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+@use '@energysage/es-ds-styles/scss/variables' as variables;
+
+.es-progress {
+    padding-bottom: v-bind(verticalContainerPaddingPx);
+    padding-top: v-bind(verticalContainerPaddingPx);
+
+    :deep(.progress-bar--with-circle::after) {
+        border: v-bind(circleBorderWidthPx) solid variables.$warm-orange;
+        box-shadow: 0 0 v-bind(circleInsetShadowSizePx) v-bind(circleInsetShadowSizePx) variables.$warm-orange inset, 0 0 v-bind(circleGlowSizePx) 0 variables.$warm-orange;
+        height: v-bind(circleDimensionPx);
+        right: v-bind(circleRightPx);
+        width: v-bind(circleDimensionPx);
+    }
+}
+</style>
--- es-ds-docs/pages/molecules/progress.vue
+++ es-ds-docs/pages/molecules/progress.vue
@@ -5,12 +5,15 @@ const compCode = ref('');
 const docCode = ref('');
 
 const propTableRows = [
-    ['value', 'Number', '20', 'Required. A value between 0 and 100 representing the progress'],
-    ['height', 'String', '0.3125rem', 'Specifies height of the progress bar'],
-    ['showValue', 'Boolean', 'false', 'When present, it allows the progress percentage visible on the progress bar'],
+    ['formatter', 'Function', '(val: number) => `${val}%`', 'Allows controlling the display of the value displayed when showValue is set to true.'],
+    ['height', 'String', '0.125rem', 'Specifies height of the progress bar'],
+    ['showCircle', 'Boolean', 'true', 'Set to false to hide the indicator circle.'],
+    ['showValue', 'Boolean', 'false', 'When present, it shows the progress bar value below the bar.'],
+    ['value', 'Number', 'n/a', 'Required. A value between 0 and 100 representing the progress'],
+    ['valueClass', 'String', "''", 'Allows control of styling for the value displayed when showValue is set to true.'],
 ];
 const propTableWidths = {
-    md: ['2', '2', '2', '6'],
+    md: ['2', '2', '3', '5'],
 };
 
 onMounted(async () => {
@@ -29,54 +32,113 @@ onMounted(async () => {
 <template>
     <div>
         <h1>Progress</h1>
-        <p class="pb-200">
+        <p>
             Extended from
             <nuxt-link
                 href="https://v3.primevue.org/progressbar/"
                 target="_blank">
-                primevue progress
+                PrimeVue ProgressBar
             </nuxt-link>
         </p>
-        <h2>Three Steps</h2>
-        <div class="mb-100">
-            <es-progress :value="0" />
+
+        <div class="my-500">
+            <h2>Basic example</h2>
+            <es-progress class="mb-100" :value="0" />
+            <es-progress class="mb-100" :value="33" />
+            <es-progress class="mb-100" :value="67" />
+            <es-progress class="mb-100" :value="100" />
         </div>
-        <div class="mb-100">
-            <es-progress :value="33" />
+
+        <div class="my-500">
+            <h2>
+                Animated transition
+            </h2>
+            <p>
+                If the value of the progress bar is changed programmatically, it will animate to the new value. Try clicking one of the buttons below to add or subtract from the progress bar's value.
+            </p>
+
+            <es-progress class="mb-100" :value="value" />
+
+            <div class="mb-100">
+                <es-button
+                    class="px-50 mr-50"
+                    size="sm"
+                    @click="value = Math.max(value - 10, 0)">
+                    <icon-minus
+                        height="16px"
+                        width="16px" />
+                    <span class="sr-only">Remove</span>
+                    10%
+                </es-button>
+                <es-button
+                    class="px-50"
+                    size="sm"
+                    @click="value = Math.min(value + 10, 100)">
+                    <icon-plus
+                        height="16px"
+                        width="16px" />
+                    <span class="sr-only">Add</span>
+                    10%
+                </es-button>
+            </div>
         </div>
-        <div class="mb-100">
-            <es-progress :value="67" />
+
+        <div class="my-500">
+            <h2>
+                Showing the value
+            </h2>
+            <p>
+                These examples show how to make the value of the progress bar visible, and how to format it to display in whatever way you want.
+            </p>
+
+            <es-progress class="mb-100" :value="value" show-value />
+
+            <es-progress class="mb-100" :formatter="(val: number) => `${(val * 10) / 100} of 10`" :value="value" show-value />
+
+            <es-progress class="mb-100" :formatter="(val: number) => `${(val * 10) / 100}/10`" :value="value" value-class="font-weight-bold" show-value />
+
+            <div class="mb-100">
+                <es-button
+                    class="px-50 mr-50"
+                    size="sm"
+                    @click="value = Math.max(value - 10, 0)">
+                    <icon-minus
+                        height="16px"
+                        width="16px" />
+                    <span class="sr-only">Remove</span>
+                    10%
+                </es-button>
+                <es-button
+                    class="px-50"
+                    size="sm"
+                    @click="value = Math.min(value + 10, 100)">
+                    <icon-plus
+                        height="16px"
+                        width="16px" />
+                    <span class="sr-only">Add</span>
+                    10%
+                </es-button>
+            </div>
         </div>
-        <div class="mb-100">
-            <es-progress :value="100" />
+
+        <div class="my-500">
+            <h2>Customized height</h2>
+            <p>The height of the progress bar is customizable. The indicator circle will scale proportionally.</p>
+            <es-progress class="mb-100" height="0.25rem" :value="0" />
+            <es-progress class="mb-100" height="0.25rem" :value="33" />
+            <es-progress class="mb-100" height="0.25rem" :value="67" />
+            <es-progress class="mb-100" height="0.25rem" :value="100" />
         </div>
-        <h2>Animated Transition</h2>
-        <div class="mb-100">
-            <es-progress :value="value" />
+
+        <div class="my-500">
+            <h2>Hide indicator circle</h2>
+            <p>This example shows how to hide the indicator circle for a simpler progress bar.</p>
+            <es-progress class="mb-200" height="0.375rem" :show-circle="false" :value="0" />
+            <es-progress class="mb-200" height="0.375rem" :show-circle="false" :value="33" />
+            <es-progress class="mb-200" height="0.375rem" :show-circle="false" :value="67" />
+            <es-progress class="mb-200" height="0.375rem" :show-circle="false" :value="100" />
         </div>
-        <div class="mb-100">
-            <es-button
-                class="px-50 mr-50"
-                size="sm"
-                @click="value = Math.max(value - 20, 0)">
-                <icon-minus
-                    height="16px"
-                    width="16px" />
-                <span class="sr-only">Remove</span>
-                20%
-            </es-button>
-            <es-button
-                class="px-50"
-                size="sm"
-                @click="value = Math.min(value + 20, 100)">
-                <icon-plus
-                    height="16px"
-                    width="16px" />
-                <span class="sr-only">Add</span>
-                20%
-            </es-button>
-        </div>
-        <p class="mb-300">value: {{ value }}</p>
+
         <div class="mb-500">
             <h2>EsProgress props</h2>
             <ds-prop-table
