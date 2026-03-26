67877e30dfae6786c5d46a66f6be7e0c30341784
ericdouglaspratt <eric.pratt@energysage.com>
2026-03-26 13:38:01 -0400

    feat: added sticky bar layout

--- es-ds-docs/app/pages/molecules/sticky-bar.vue
+++ es-ds-docs/app/pages/molecules/sticky-bar.vue
@@ -2,18 +2,18 @@
 const LOREM_TEXT = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In tellus integer feugiat scelerisque varius. Risus in hendrerit gravida rutrum. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. Mi tempus imperdiet nulla malesuada pellentesque elit.';
 
 const samplePageContent = Array(10).fill(LOREM_TEXT);
+
+definePageMeta({
+    layout: 'sticky-bar',
+});
 </script>
 
 <template>
     <div>
         <h1>Sticky bar</h1>
-        <es-sticky-bar>
-            <es-container>
-                <p class="py-100 py-lg-200 mb-0">
-                    test content
-                </p>
-            </es-container>
-        </es-sticky-bar>
+        <p>
+            The sticky bar is intended for use with navigation bars. When scrolling down, it will automatically disappear as soon as you scroll past the height of the nav bar itself. It will come back when you scroll up slightly.
+        </p>
         <p v-for="(content, index) in samplePageContent" :key="index" class="my-400">
             {{ content }}
         </p>
