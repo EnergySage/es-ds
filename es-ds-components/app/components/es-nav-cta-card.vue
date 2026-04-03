9e7fb42c8c7cdee4aca5746408ca77b5ec52a0bd
ericdouglaspratt <eric.pratt@energysage.com>
2026-04-03 11:19:30 -0400

    fix: formatting

--- es-ds-components/app/components/es-nav-cta-card.vue
+++ es-ds-components/app/components/es-nav-cta-card.vue
@@ -20,7 +20,9 @@ withDefaults(defineProps<Props>(), {
         :href="href"
         :target="target"
         variant="interactive">
-        <div v-if="$slots.icon" class="mr-100">
+        <div
+            v-if="$slots.icon"
+            class="mr-100">
             <div class="es-nav-cta-card-icon bg-blue-50 p-50 position-relative rounded-xs text-blue-600">
                 <slot name="icon" />
             </div>
