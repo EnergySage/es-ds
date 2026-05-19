8675cc30a6bf66d322fbdeb69e438211fa7a0e5c
ericdouglaspratt <eric.pratt@energysage.com>
2026-05-19 10:04:15 -0400

    docs: mention Reka UI on index page

--- es-ds-docs/app/pages/index.vue
+++ es-ds-docs/app/pages/index.vue
@@ -6,16 +6,20 @@
             <a
                 href="https://getbootstrap.com/docs/4.6/getting-started/introduction/"
                 target="_blank">
-                Bootstrap 4
-            </a>
-            and
+                Bootstrap 4</a
+            >,
+            <a
+                href="https://reka-ui.com/"
+                target="_blank">
+                Reka UI</a
+            >, and
             <a
                 href="https://v3.primevue.org/introduction/"
                 target="_blank">
-                PrimeVue v3
-            </a>
+                PrimeVue v3</a
+            >
             projects. If something is documented in Bootstrap but not here, it is still very likely you can use it in
-            our system. If a component does not exist here but exists in PrimeVue, you may use it, but it will not have
+            our system. If a component does not exist here but exists in Reka UI or PrimeVue, you may use it, but it will not have
             any styles.
         </p>
         <ul>
