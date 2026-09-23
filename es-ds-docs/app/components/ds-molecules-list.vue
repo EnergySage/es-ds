3d61cabccd95d2c67f2cea29ff185b83030fb97f
ericdouglaspratt <eric.pratt@energysage.com>
2026-09-23 17:20:40 -0400

    Reapply "fix: restore autocomplete to molecules list"
    This reverts commit 5f6323052e2d6f8b6f8dea26adde8c9a355110a7.

--- es-ds-docs/app/components/ds-molecules-list.vue
+++ es-ds-docs/app/components/ds-molecules-list.vue
@@ -3,9 +3,9 @@
         <li>
             <ds-link to="/molecules/accordion"> Accordion </ds-link>
         </li>
-        <!--<li>
+        <li>
             <ds-link to="/molecules/autocomplete"> Autocomplete </ds-link>
-        </li>--->
+        </li>
         <li>
             <ds-link to="/molecules/badge"> Badge </ds-link>
         </li>
